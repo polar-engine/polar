23955f1ab30c883a06e8a13e8404eda9c22bfd3d
Shockk <shokku.ra@gmail.com>
2014-11-14 05:33:23 +0000

    Moved SDL includes.

--- Sub-Terra/SubTerra.cpp
+++ Sub-Terra/SubTerra.cpp
@@ -1,12 +1,12 @@
 #include "SubTerra.h"
 #include "Polar.h"
-#include "NoRenderer.h"
+#include "GL32Renderer.h"
 #include "StdOutComponent.h"
 #include "DrawableComponent.h"
 
 void SubTerra::Run(const std::vector<const std::string> &args) {
 	Polar engine;
-	engine.AddSystem<NoRenderer>();
+	engine.AddSystem<GL32Renderer>();
 	engine.AddObject({
 		new StdOutComponent("hello world"),
 		new DrawableComponent()
