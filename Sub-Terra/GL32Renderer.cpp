b9ceb869f1ebbe61ee6ecf04ec5946f1a0a56ad4
Shockk <shokku.ra@gmail.com>
2014-11-16 00:00:56 +0000

    Lots of changes, broke event manager temporarily

--- Sub-Terra/GL32Renderer.cpp
+++ Sub-Terra/GL32Renderer.cpp
@@ -4,16 +4,21 @@
 bool GL32Renderer::IsSupported() {
 	GL32Renderer renderer;
 	try {
-		renderer.Init();
-		/* TODO: check OpenGL major/minor version */
+		auto e = std::runtime_error(BASEFILE);
+		renderer.InitGL();
+		GLint major, minor;
+		if(!GL(glGetIntegerv(GL_MAJOR_VERSION, &major))) { throw e; }
+		if(!GL(glGetIntegerv(GL_MINOR_VERSION, &minor))) { throw e; }
+		if(major != 3 || minor != 2) { throw e; }
 		renderer.Destroy();
 	} catch(std::exception &) {
+		renderer.Destroy();
 		return false;
 	}
 	return true;
 }
 
-void GL32Renderer::Init() {
+void GL32Renderer::InitGL() {
 	auto e = std::runtime_error(BASEFILE);
 	if(!SDL(SDL_Init(SDL_INIT_EVERYTHING))) { throw e; }
 	if(!SDL(SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3))) { throw e; }
@@ -25,6 +30,31 @@ void GL32Renderer::Init() {
 	if(!SDL(window = SDL_CreateWindow("Polar Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN))) { throw e; }
 	if(!SDL(context = SDL_GL_CreateContext(window))) { throw e; }
 	if(!SDL(SDL_GL_SetSwapInterval(1))) { throw e; }
+
+	glewExperimental = GL_TRUE;
+
+	GLenum err;
+	GL(err = glewInit());
+
+	if(err != GLEW_OK) {
+		ERROR("GLEW: glewInit failed");
+		throw e;
+	}
+}
+
+void GL32Renderer::Init() {
+	InitGL();
+	/*
+	eventManager->ListenFor("renderer", "bgcolor", [] (void *p) {
+		DEBUG(typeid(p).name());
+		assert(typeid(p) == typeid(std::tuple<float, float, float, float>));
+		auto &color = *reinterpret_cast<std::tuple<float, float, float, float> *>(p);
+		GL(glClearColor(std::get<0>(color), std::get<1>(color), std::get<2>(color), std::get<3>(color)));
+	});
+
+	auto color = std::make_tuple(0.02f, 0.05f, 0.1f, 0.0f);
+	eventManager->FireIn("renderer", "bgcolor", &color);
+	*/
 }
 
 void GL32Renderer::Update(int) {
@@ -32,6 +62,9 @@ void GL32Renderer::Update(int) {
 	while(SDL_PollEvent(&event)) {
 		HandleSDL(event);
 	}
+
+	GL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+	SDL(SDL_GL_SwapWindow(window));
 }
 
 void GL32Renderer::Destroy() {
