908eadf3ad7c84cfc8ad5439fb27eb74d3be69ac
Shockk <shokku.ra@gmail.com>
2014-11-23 00:07:14 +0000

    Wrote template endian functions
    Changed size of TextAsset length to 4 bytes
    Added explicit static_cast to suppress data loss warning

--- include/TextAsset.h
+++ include/TextAsset.h
@@ -4,20 +4,20 @@
 
 class TextAsset : public Asset {
 public:
-	const uint64_t length;
+	const uint32_t length;
 	const std::string text;
 
 	static std::string Type() { return "text"; }
 	static TextAsset Load(const std::string &data) {
-		const uint64_t dataLength = data.length();
-		if(dataLength < 8) { throw std::runtime_error("missing data length"); }
-		const uint64_t length = hton64(*reinterpret_cast<const uint64_t *>(data.c_str()));
-		if(length > dataLength - 8) { throw std::runtime_error("invalid data length"); }
-		return TextAsset(length, data.substr(8, static_cast<unsigned int>(length)));
+		const uint32_t dataLength = data.length();
+		if(dataLength < 4) { throw std::runtime_error("missing data length"); }
+		const uint32_t length = swapbe(*reinterpret_cast<const uint32_t *>(data.c_str()));
+		if(length > dataLength - 4) { throw std::runtime_error("invalid data length"); }
+		return TextAsset(length, data.substr(4, static_cast<unsigned int>(length)));
 	}
-	TextAsset(const uint64_t length, const std::string &text) : Asset("text"), length(length), text(text) {}
+	TextAsset(const uint32_t length, const std::string &text) : Asset("text"), length(length), text(text) {}
 	std::string Save() const override final {
-		const uint64_t beLength = ntoh64(length);
-		return std::string(reinterpret_cast<const char *>(&beLength), 8) + text;
+		const uint32_t beLength = swapbe(length);
+		return std::string(reinterpret_cast<const char *>(&beLength), 4) + text;
 	}
 };
--- include/endian.h
+++ include/endian.h
@@ -1,9 +1,19 @@
 #pragma once
 
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <Winsock2.h>
-#define hton64 htonll
-#define ntoh64 ntohll
-#endif
+template<typename T> inline T swapbe(T n) {
+	T r = 0;
+	auto data = reinterpret_cast<unsigned char *>(&n);
+	for(unsigned int i = 0; i < sizeof(T); ++i) {
+		r |= data[sizeof(T) - i - 1] << (i * 8);
+	}
+	return r;
+}
+
+template<typename T> inline T swaple(T n) {
+	T r = 0;
+	auto data = reinterpret_cast<unsigned char *>(&n);
+	for(unsigned int i = 0; i < sizeof(T); ++i) {
+		r |= data[i] << (i * 8);
+	}
+	return r;
+}
