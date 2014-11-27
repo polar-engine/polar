de832d5172a45c42b3448d0a99014ccec24ad1af
shockk <shokku.ra@gmail.com>
2014-11-27 06:10:14 +0000

    Added virtual destructor to Asset

--- include/Asset.h
+++ include/Asset.h
@@ -9,5 +9,6 @@ public:
 	const std::string type = Type();
 	static std::string Type() { return "text"; }
 	Asset(const std::string &type) : type(type) {}
+	virtual ~Asset() {}
 	virtual std::string Save() const = 0;
 };
