9c95dcde4bd9f68f6114364c5fda51a82278a24d
Shockk <shokku.ra@gmail.com>
2014-12-04 10:34:04 +0000

    Added const qualifier to ctor arguments of position and orientation components

--- Sub-Terra/include/PositionComponent.h
+++ Sub-Terra/include/PositionComponent.h
@@ -6,7 +6,7 @@ class PositionComponent : public Component {
 public:
 	Point position;
 	PositionComponent() : position(0, 0, 0, 1) {}
-	PositionComponent(Point &position) : position(position) {}
+	PositionComponent(const Point &position) : position(position) {}
 	PositionComponent(Point &&position) : position(position) {}
 	virtual ~PositionComponent() override {}
 };
