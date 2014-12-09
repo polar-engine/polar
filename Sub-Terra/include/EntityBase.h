e18ae28450ec023b88934ebdb3db5f44df679100
Shockk <shokku.ra@gmail.com>
2014-12-09 10:58:06 +0000

    Inlined a bunch of methods.

--- Sub-Terra/include/EntityBase.h
+++ Sub-Terra/include/EntityBase.h
@@ -6,38 +6,38 @@ private:
 	ComponentsType components;
 public:
 	virtual ~EntityBase() {}
-	template<typename T> void Add() {
+	template<typename T> inline void Add() {
 		Add(new T());
 	}
 
-	template<typename T, typename ...Ts> void Add(Ts && ...args) {
+	template<typename T, typename ...Ts> inline void Add(Ts && ...args) {
 		Add(new T(std::forward<Ts>(args)...));
 	}
 
-	template<typename T> void Add(T *component) {
+	template<typename T> inline void Add(T *component) {
 		static_assert(std::is_base_of<C, T>::value, "EntityBase::Add requires object of correct type");
 		components.emplace(&typeid(T), std::unique_ptr<C>(component));
 	}
 
-	template<typename T> void Remove() {
+	template<typename T> inline void Remove() {
 		static_assert(std::is_base_of<C, T>::value, "EntityBase::Remove requires object of correct type");
 		components.erase(&typeid(T));
 	}
 
-	template<typename T> bool Has() const {
+	template<typename T> inline bool Has() const {
 		static_assert(std::is_base_of<C, T>::value, "EntityBase::Has requires object of correct type");
 		return Has(&typeid(T));
 	}
 
-	bool Has(const std::type_info *ti) const {
+	inline bool Has(const std::type_info *ti) const {
 		return components.find(ti) != components.end();
 	}
 
-	template<typename T> T * Get() const {
+	template<typename T> inline T * Get() const {
 		static_assert(std::is_base_of<C, T>::value, "EntityBase::Get requires object of correct type");
 		auto it = components.find(&typeid(T));
 		if(it == components.end()) { return nullptr; } else { return static_cast<T *>(it->second.get()); }
 	}
 
-	const ComponentsType * const Get() const { return &components; }
+	inline const ComponentsType * const Get() const { return &components; }
 };
