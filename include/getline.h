586f7a674ea3ab19206ec5fec952f44ebd293a95
Shockk <shokku.ra@gmail.com>
2014-12-13 07:20:46 +0000

    Wrote custom getline function to correctly handle all line ending types.

---
+++ include/getline.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <istream>
+#include <string>
+#include <sstream>
+
+std::istream & getline(std::istream &is, std::string &line) {
+	line.clear();
+	std::istream::sentry sentry(is, true);
+	std::streambuf *sb = is.rdbuf();
+
+	while(true) {
+		int c = sb->sbumpc();
+		switch(c) {
+		case '\n':
+			return is;
+		case '\r':
+			if(sb->sgetc() == '\n') { sb->sbumpc(); }
+			return is;
+		case EOF:
+			if(line.empty()) { is.setstate(std::ios::eofbit); }
+			return is;
+		default:
+			line += static_cast<char>(c);
+			break;
+		}
+	}
+}
