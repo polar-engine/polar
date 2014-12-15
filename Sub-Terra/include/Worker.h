b3d82eda0d0c33c56fe1aa1aea3b11bba38b5cdc
Shockk <shokku.ra@gmail.com>
2014-12-15 21:51:07 +0000

    Rewrote Worker to use Atomic jobs.

--- Sub-Terra/include/Worker.h
+++ Sub-Terra/include/Worker.h
@@ -1,14 +1,19 @@
 #pragma once
 
+/* Windows.h and stuff */
+#ifdef AddJob
+#undef AddJob
+#endif
+
 #include "Job.h"
+
+typedef std::priority_queue<Job> JobsType;
 
 class Worker {
 private:
 	std::thread _thread;
+	Atomic<JobsType> jobs;
 public:
-	std::mutex jobsLock;
-	std::priority_queue<Job> jobs;
-
 	void Start();
 	bool Join();
 	void AddJob(Job);
