26c34f8ab853556c76d497e77d3e8bc860b3a2b3
Shockk <shokku.ra@gmail.com>
2014-12-18 22:10:19 +0000

    Overhauled DeltaTicks class

--- Sub-Terra/include/DeltaTicks.h
+++ Sub-Terra/include/DeltaTicks.h
@@ -2,18 +2,44 @@
 
 class DeltaTicks {
 private:
-	DeltaTicksBase base;
+	DeltaTicksBase value;
 public:
 	typedef float seconds_type;
 
-	DeltaTicks(const DeltaTicksBase &base) : base(base) {}
-	DeltaTicks(const DeltaTicksBase::rep ticks) : base(ticks) {}
+	DeltaTicks() {}
+	DeltaTicks(const DeltaTicksBase &value) : value(value) {}
+	DeltaTicks(const DeltaTicksBase::rep ticks) : value(ticks) {}
 
 	inline DeltaTicksBase::rep Ticks() const {
-		return base.count();
+		return value.count();
 	}
 
 	inline seconds_type Seconds() const {
-		return base.count() / static_cast<seconds_type>(ENGINE_TICKS_PER_SECOND);
+		return value.count() / static_cast<seconds_type>(ENGINE_TICKS_PER_SECOND);
 	}
+
+	inline bool operator<(const DeltaTicksBase &rhs) { return value < rhs; }
+	inline bool operator<(const DeltaTicks &rhs) { return *this < rhs.value; }
+	inline bool operator>(const DeltaTicksBase &rhs) { return value < rhs; }
+	inline bool operator>(const DeltaTicks &rhs) { return *this < rhs.value; }
+	inline bool operator<=(const DeltaTicksBase &rhs) { return !(*this > rhs); }
+	inline bool operator<=(const DeltaTicks &rhs) { return !(*this > rhs.value); }
+	inline bool operator>=(const DeltaTicksBase &rhs) { return !(*this < rhs); }
+	inline bool operator>=(const DeltaTicks &rhs) { return !(*this < rhs.value); }
+
+	inline DeltaTicks & operator+=(const DeltaTicksBase &rhs) {
+		value += rhs;
+		return *this;
+	}
+	inline DeltaTicks & operator+=(const DeltaTicks &rhs) { return *this += rhs.value; }
+	inline friend DeltaTicks operator+(DeltaTicks lhs, const DeltaTicksBase &rhs) { return lhs += rhs; }
+	inline friend DeltaTicks operator+(DeltaTicks lhs, const DeltaTicks &rhs) { return lhs += rhs; }
+
+	inline DeltaTicks & operator-=(const DeltaTicksBase &rhs) {
+		value -= rhs;
+		return *this;
+	}
+	inline DeltaTicks & operator-=(const DeltaTicks &rhs) { return *this -= rhs.value; }
+	inline friend DeltaTicks operator-(DeltaTicks lhs, const DeltaTicksBase &rhs) { return lhs -= rhs; }
+	inline friend DeltaTicks operator-(DeltaTicks lhs, const DeltaTicks &rhs) { return lhs -= rhs; }
 };
