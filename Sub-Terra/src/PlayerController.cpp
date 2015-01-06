0e6e97abdb16ea6df450d9c56014944ceaf5cfee
Shockk <shokku.ra@gmail.com>
2015-01-06 20:38:40 +0000

    Minor changes to player controller

--- Sub-Terra/src/PlayerController.cpp
+++ Sub-Terra/src/PlayerController.cpp
@@ -22,14 +22,15 @@ void PlayerController::Init() {
 	};
 
 	object = new Object();
-	object->Add<PositionComponent>(Point(3, 2, 5, 1));
+	object->Add<PositionComponent>();
 	object->Add<OrientationComponent>();
 	object->Add<ModelComponent>(triangles);
 	InitObject();
 	engine->AddObject(object);
 
+	/* gravity */
 	auto pos = object->Get<PositionComponent>();
-	pos->position.Derivative(1) = Point(0, -9.8f, 0, 1);
+	//pos->position.Derivative(1) = Point(0, -9.8f, 0, 1);
 }
 
 void PlayerController::Update(DeltaTicks &dt, std::vector<Object *> &objects) {
@@ -37,7 +38,8 @@ void PlayerController::Update(DeltaTicks &dt, std::vector<Object *> &objects) {
 	auto orient = object->Get<OrientationComponent>();
 
 	auto rel = glm::normalize(glm::fvec4(moveLeft ? -1 : 0 + moveRight ? 1 : 0, 0, moveForward ? -1 : 0 + moveBackward ? 1 : 0, 1));
-	auto abs = (glm::inverse(orient->orientation) * rel) * 10.0f;
+	auto abs = (glm::inverse(orient->orientation) * rel) * 2.0f * 16.0f;
 	pos->position.Derivative()->x = abs.x;
+	pos->position.Derivative()->y = abs.y;
 	pos->position.Derivative()->z = abs.z;
 }
