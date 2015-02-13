12bce5e1e7e5830b77b78d7d5f34506c353fedba
Shockk <shokku.ra@gmail.com>
2015-02-13 15:58:07 +0000

    PlayerController: Decrease movement speed

--- Sub-Terra/src/PlayerController.cpp
+++ Sub-Terra/src/PlayerController.cpp
@@ -38,7 +38,7 @@ void PlayerController::Update(DeltaTicks &dt, std::vector<Object *> &objects) {
 	auto orient = object->Get<OrientationComponent>();
 
 	auto rel = glm::normalize(glm::fvec4(moveLeft ? -1 : 0 + moveRight ? 1 : 0, 0, moveForward ? -1 : 0 + moveBackward ? 1 : 0, 1));
-	auto abs = (glm::inverse(orient->orientation) * rel) * 2.0f * 16.0f;
+	auto abs = (glm::inverse(orient->orientation) * rel) * 2.0f * 16.0f * 0.25f;
 	pos->position.Derivative()->x = abs.x;
 	pos->position.Derivative()->y = abs.y;
 	pos->position.Derivative()->z = abs.z;
