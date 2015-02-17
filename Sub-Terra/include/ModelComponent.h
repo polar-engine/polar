f2c9a7d933063bef7d5abf62c6ab1d028b53699e
Shockk <shokku.ra@gmail.com>
2015-02-17 20:41:33 +0000

    Save memory by not storing normals after GPU upload

--- Sub-Terra/include/ModelComponent.h
+++ Sub-Terra/include/ModelComponent.h
@@ -9,34 +9,38 @@ public:
 
 	GeometryType type;
 	PointsType points;
-	PointsType normals;
 
 	ModelComponent() : type(GeometryType::None) {}
-	ModelComponent(GeometryType type, const PointsType &points, const PointsType &normals)
-		: type(type), points(points), normals(normals) {}
+	ModelComponent(GeometryType type, const PointsType &points) : type(type), points(points) {}
 	ModelComponent(const TrianglesType &triangles) : type(GeometryType::Triangles) {
 		auto size = triangles.size() * 3;
 		points.reserve(size);
-		normals.reserve(size);
 		for(TrianglesType::size_type i = 0; i < triangles.size(); ++i) {
 			auto &triangle = triangles.at(i);
 			points.emplace_back(std::get<0>(triangle));
 			points.emplace_back(std::get<1>(triangle));
 			points.emplace_back(std::get<2>(triangle));
+		}
+	}
 
-			Point3 deltaPos1 = std::get<1>(triangle) - std::get<0>(triangle);
-			Point3 deltaPos2 = std::get<2>(triangle) - std::get<0>(triangle);
-
-			auto normal = CalculateNormal(triangle);
+	PointsType CalculateNormals() const {
+		PointsType normals;
+		normals.reserve(points.size());
+		for(PointsType::size_type i = 0; i < points.size(); i += 3) {
+			auto normal = CalculateNormal(points[i], points[i + 1], points[i + 2]);
 			normals.emplace_back(normal);
 			normals.emplace_back(normal);
 			normals.emplace_back(normal);
 		}
+		return normals;
 	}
 
 	static Point3 CalculateNormal(const Triangle &triangle) {
-		Point3 deltaPos1 = std::get<1>(triangle) -std::get<0>(triangle);
-		Point3 deltaPos2 = std::get<2>(triangle) -std::get<0>(triangle);
+		return CalculateNormal(std::get<0>(triangle), std::get<1>(triangle), std::get<2>(triangle));
+	}
+	static Point3 CalculateNormal(const Point3 &p1, const Point3 &p2, const Point3 &p3) {
+		Point3 deltaPos1 = p2 - p1;
+		Point3 deltaPos2 = p3 - p1;
 
 		Point3 normal;
 		normal.x = deltaPos1.y * deltaPos2.z - deltaPos1.z * deltaPos2.y;
