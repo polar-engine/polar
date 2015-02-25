49b6ec98e12b18d409354386db7dcc7811e7e421
Shockk <shokku.ra@gmail.com>
2015-02-25 04:25:11 +0000

    World: Use engine bimap to lookup camera
    This patch removes the exceptional tracking done by World of a single object containing a player camera component and instead uses the new bimap system to lookup and loop over all objects with a player camera component.

--- Sub-Terra/include/World.h
+++ Sub-Terra/include/World.h
@@ -36,12 +36,10 @@ private:
 	const glm::ivec3 chunkSize;
 	OpenSimplexNoise noise = OpenSimplexNoise(std::random_device()());
 	Atomic<ChunksType> chunks;
-	Object *cameraObj = nullptr;
 protected:
 	void Init() override final;
 	void Update(DeltaTicks &) override final;
 	void Destroy() override final;
-	void ObjectAdded(Object *) override final;
 public:
 	static bool IsSupported() { return true; }
 	World(Polar *engine, const unsigned char chunkWidth, const unsigned char chunkHeight, const unsigned char chunkDepth)
