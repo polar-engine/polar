640089a3b7939cccfe433674099df23747eff46e
Shockk <shokku.ra@gmail.com>
2015-02-27 03:27:52 +0000

    Rework asset pipeline (part 2)

---
+++ include/ShaderProgramAsset.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <vector>
+#include "debug.h"
+#include "Asset.h"
+#include "TextAsset.h"
+#include "ListAsset.h"
+
+/******************** ShaderProgramInputAsset ********************
+* TextAsset   key name
+* TextAsset   buffer name
+*/
+struct ShaderProgramInputAsset : Asset {
+	TextAsset key;
+	TextAsset name;
+
+	ShaderProgramInputAsset() {}
+	ShaderProgramInputAsset(TextAsset key, TextAsset name) : key(key), name(name) {}
+};
+
+inline std::ostream & operator<<(std::ostream &os, ShaderProgramInputAsset asset) {
+	os << asset.key;
+	os << asset.name;
+	return os;
+}
+
+inline std::istream & operator>>(std::istream &is, ShaderProgramInputAsset &asset) {
+	is >> asset.key;
+	is >> asset.name;
+	return is;
+}
+
+enum class ProgramOutputType : uint8_t {
+	Invalid = 0,
+	Color   = 1,
+	Depth   = 2
+};
+
+/******************** ShaderProgramOutputAsset ********************
+ * 1           type
+ * TextAsset   key name
+ * TextAsset   buffer name
+ */
+struct ShaderProgramOutputAsset : Asset {
+	ProgramOutputType type;
+	TextAsset key;
+	TextAsset name;
+
+	ShaderProgramOutputAsset() {}
+	ShaderProgramOutputAsset(ProgramOutputType type, TextAsset key, TextAsset name) : type(type), key(key), name(name) {}
+};
+
+inline std::ostream & operator<<(std::ostream &os, ShaderProgramOutputAsset asset) {
+	os << static_cast<uint8_t>(asset.type);
+	os << asset.key;
+	os << asset.name;
+	return os;
+}
+
+inline std::istream & operator>>(std::istream &is, ShaderProgramOutputAsset &asset) {
+	asset.type = static_cast<ProgramOutputType>(is.get());
+	is >> asset.key;
+	is >> asset.name;
+	return is;
+}
+
+enum class ShaderType : uint8_t {
+	Invalid  = 0,
+	Vertex   = 1,
+	Fragment = 2
+};
+
+/******************** ShaderAsset ********************
+ * 1           type
+ * TextAsset   source
+ */
+struct ShaderAsset : Asset {
+	ShaderType type;
+	TextAsset source;
+
+	ShaderAsset() {}
+	ShaderAsset(ShaderType type, TextAsset source) : type(type), source(source) {}
+};
+
+inline std::ostream & operator<<(std::ostream &os, ShaderAsset asset) {
+	os << static_cast<uint8_t>(asset.type);
+	os << asset.source;
+	return os;
+}
+
+inline std::istream & operator>>(std::istream &is, ShaderAsset &asset) {
+	asset.type = static_cast<ShaderType>(is.get());
+	is >> asset.source;
+	return is;
+}
+
+/******************** ShaderProgramAsset ********************
+ * ListAsset<ShaderProgramInputAsset>    inputs of the program
+ * ListAsset<ShaderProgramOutputAsset>   outputs of the program
+ * ListAsset<ShaderAsset>                shaders of the program
+ */
+struct ShaderProgramAsset : Asset {
+	ListAsset<ShaderProgramInputAsset> ins;
+	ListAsset<ShaderProgramOutputAsset> outs;
+	ListAsset<ShaderAsset> shaders;
+
+	ShaderProgramAsset() {}
+};
+
+inline std::ostream & operator<<(std::ostream &os, ShaderProgramAsset asset) {
+	os << asset.ins;
+	os << asset.outs;
+	os << asset.shaders;
+	return os;
+}
+
+inline std::istream & operator>>(std::istream &is, ShaderProgramAsset &asset) {
+	is >> asset.ins;
+	is >> asset.outs;
+	is >> asset.shaders;
+	return is;
+}
+
+template<> inline std::string AssetName<ShaderProgramAsset>() { return "ShaderProgram"; }
