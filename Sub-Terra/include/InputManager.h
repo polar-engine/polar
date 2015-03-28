126e963337db9cfa64a0b15be0e1442d46c485f9
Shockk <shokku.ra@gmail.com>
2015-03-28 21:41:49 +0000

    Use bimap for InputManager handlers
    This patch replaces the unordered_maps used in InputManager to store key handlers with bimaps of Key<=>IDs and handlers stored in per-relation additional info.

--- Sub-Terra/include/InputManager.h
+++ Sub-Terra/include/InputManager.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <set>
+#include <boost/container/set.hpp>
+#include <boost/bimap.hpp>
+#include <boost/bimap/multiset_of.hpp>
+#include <boost/bimap/unordered_set_of.hpp>
 #include "System.h"
 #include "Key.h"
 
@@ -10,12 +13,21 @@ typedef std::function<void(Key, const DeltaTicks &)> WhenKeyHandler;
 typedef std::function<void(const Point2 &)> MouseMoveHandler;
 
 class InputManager : public System {
+public:
+	typedef std::uint_fast32_t IDType;
+	template<typename _Handler> using KeyHandlerBimap = boost::bimap<
+		boost::bimaps::multiset_of<Key>,
+		boost::bimaps::unordered_set_of<IDType>,
+		boost::bimaps::set_of_relation<>,
+		boost::bimaps::with_info<_Handler>
+	>;
 private:
-	std::set<Key> keys;
-	std::unordered_multimap<Key, OnKeyHandler, KeyHash, KeyEqual> onKeyHandlers;
-	std::unordered_multimap<Key, AfterKeyHandler, KeyHash, KeyEqual> afterKeyHandlers;
-	std::unordered_multimap<Key, WhenKeyHandler, KeyHash, KeyEqual> whenKeyHandlers;
+	boost::container::set<Key> keys;
+	KeyHandlerBimap<OnKeyHandler> onKeyHandlers;
+	KeyHandlerBimap<AfterKeyHandler> afterKeyHandlers;
+	KeyHandlerBimap<WhenKeyHandler> whenKeyHandlers;
 	std::vector<MouseMoveHandler> mouseMoveHandlers;
+	IDType nextID = 1;
 protected:
 	void Init() override final;
 	void Update(DeltaTicks &) override final;
@@ -23,13 +35,13 @@ public:
 	static bool IsSupported() { return true; }
 	InputManager(Polar *engine) : System(engine) {}
 	void On(Key key, const OnKeyHandler &handler) {
-		onKeyHandlers.emplace(key, handler);
+		onKeyHandlers.insert(KeyHandlerBimap<OnKeyHandler>::value_type(key, nextID++, handler));
 	}
 	void After(Key key, const AfterKeyHandler &handler) {
-		afterKeyHandlers.emplace(key, handler);
+		afterKeyHandlers.insert(KeyHandlerBimap<AfterKeyHandler>::value_type(key, nextID++, handler));
 	}
 	void When(Key key, const WhenKeyHandler &handler) {
-		whenKeyHandlers.emplace(key, handler);
+		whenKeyHandlers.insert(KeyHandlerBimap<WhenKeyHandler>::value_type(key, nextID++, handler));
 	}
 	void OnMouseMove(const MouseMoveHandler &handler) {
 		mouseMoveHandlers.emplace_back(handler);
