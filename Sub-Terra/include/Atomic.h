7719b4be0cc599fa63f7282b261586380c221001
Shockk <shokku.ra@gmail.com>
2015-03-29 21:15:53 +0100

    Add condition variable to Atomic

--- Sub-Terra/include/Atomic.h
+++ Sub-Terra/include/Atomic.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef _WIN32
+#ifndef _WIN32
 #include <Windows.h>
 
 template<typename T> class Atomic {
@@ -32,21 +32,37 @@ public:
 
 #else
 
+#include <mutex>
+#include <condition_variable>
+
 template<typename T> class Atomic {
 public:
-	typedef std::recursive_mutex mutex_type;
+	typedef std::mutex mutex_type;
 private:
 	T value;
 	mutex_type mutex;
+	std::condition_variable cv;
 public:
 	template<typename ...Ts> Atomic(Ts && ...args) : value(std::forward<Ts>(args)...) {}
+
+	inline void Notify() {
+		cv.notify_one();
+	}
+
+	inline void Wait(const std::function<bool(T &)> &pred, const std::function<void(T &)> &fn) {
+		std::unique_lock<std::mutex> lock(mutex);
+		while(!pred(value)) {
+			cv.wait(lock);
+		}
+		fn(value);
+	}
 
 	inline void With(const std::function<void(T &)> &fn) {
 		std::lock_guard<mutex_type> lock(mutex);
 		fn(value);
 	}
 
-	template<typename Ret> inline Ret With(const std::function<Ret(T &)> &fn) {
+	template<typename _Ret> inline _Ret With(const std::function<_Ret(T &)> &fn) {
 		std::lock_guard<mutex_type> lock(mutex);
 		return fn(value);
 	}
