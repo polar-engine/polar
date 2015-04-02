0ed92c321cd618f7b02427796ae9075fe1a9b0dd
Shockk <shokku.ra@gmail.com>
2015-04-02 17:58:54 +0100

    Various improvements and optimizations to World

--- Sub-Terra/include/World.h
+++ Sub-Terra/include/World.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <boost/container/vector.hpp>
 #include <boost/unordered_map.hpp>
 #include "System.h"
 #include "OpenSimplexNoise.h"
@@ -30,6 +31,57 @@ public:
 	World(Polar *engine, const unsigned char chunkWidth, const unsigned char chunkHeight, const unsigned char chunkDepth)
 		: System(engine), chunkSize(chunkWidth, chunkHeight, chunkDepth) {}
 	~World();
-	std::vector<bool> GenerateChunk(const Point3 &&) const;
-	bool GenerateBlock(const Point3 &&) const;
+
+	inline Point3 BlockCoordForPos(Point3 &pos) const {
+		return glm::floor(pos / blockSize);
+	}
+	inline Point3 ChunkCoordForPos(Point3 &pos) const {
+		return glm::floor(pos / (blockSize * Point3(chunkSize)));
+	}
+	inline boost::container::vector<bool> GenerateChunk(const Point3 &p) const {
+		boost::container::vector<bool> blocks;
+		blocks.resize(chunkSize.x * chunkSize.y * chunkSize.z);
+
+		Point3 actual = p * Point3(blockSize.x * chunkSize.x, blockSize.y * chunkSize.y, blockSize.z * chunkSize.z);
+
+		int i = 0;
+		for(float z = 0; z < blockSize.z * chunkSize.z; z += blockSize.z) {
+			for(float x = 0; x < blockSize.x * chunkSize.x; x += blockSize.x) {
+				for(float y = 0; y < blockSize.y * chunkSize.y; y += blockSize.y) {
+					blocks.at(i++) = GenerateBlock(actual + Point3(x, y, z));
+				}
+			}
+		}
+		return blocks;
+	}
+
+	inline bool GenerateBlock(const Point3 &&p) const {
+		/* periodic 'normal distribution' function */
+		auto fDensity = [] (const double x) {
+			return 1.0 - glm::abs(glm::sin(x));
+		};
+
+		const float scale1 = 17.0f;
+		const float scale2 = scale1 / 17.0f * 8.0f;
+		auto eval1 = noise.eval(p.x / scale1, p.y * 1.6f / scale1, p.z / scale1);
+		auto eval2 = noise.eval(p.x / scale2, p.y / scale2 / 3.0f, p.z / scale2 / 5.0f);
+
+		/* cave systems */
+		auto result1 = (
+			eval1 > (fDensity(p.x / scale1 / 32.0f) - 1) * 1.5f + 0.1f ||
+			eval1 > (fDensity(p.y / scale1 / 10.0f) - 1) * 1.5f + 0.1f ||
+			eval1 > (fDensity(p.z / scale1 / 32.0f) - 1) * 1.5f + 0.1f
+			);
+		/* cave system crevices */
+		auto result2 = (
+			eval1 > (fDensity(p.x / scale1 / 32.0f) - 1) * 1.5f + 0.1f ||
+			eval1 > (fDensity(p.y / scale1 / 10.0f + noise.eval(p.x / scale1 / 60.0f, p.z / scale1 / 60.0f) * 100.0f) - 1) * 1.5f + 0.1f ||
+			eval1 > (fDensity(p.z / scale1 / 32.0f) - 1) * 1.5f + 0.1f
+			);
+		/* crevices */
+		auto result3 = eval2 > -0.75;
+
+		/* AND results so there is only a block if no results dictate no block */
+		return result1 && result2 && result3;
+	}
 };
--- Sub-Terra/src/World.cpp
+++ Sub-Terra/src/World.cpp
@@ -17,19 +17,18 @@ void World::Update(DeltaTicks &) {
 
 		auto pos = engine->GetComponent<PositionComponent>(object);
 		if(pos != nullptr) {
-			auto chunkSizeF = Point3(chunkSize);
-			auto keyBase = glm::ivec3(glm::floor(pos->position.Get() / chunkSizeF));
+			auto chunkCoord = ChunkCoordForPos(pos->position.Get());
 			auto jobM = engine->systems.Get<JobManager>().lock();
 
 			/* clean up chunks outside distance */
-			chunks.With([this, jobM, &keyBase] (ChunksType &chunks) {
+			chunks.With([this, jobM, &chunkCoord] (ChunksType &chunks) {
 				for(auto it = chunks.begin(); it != chunks.end();) {
 					auto &keyTuple = it->first;
 					auto obj = std::get<1>(it->second);
 
-					if(abs(static_cast<int>(std::get<0>(keyTuple)) - keyBase.x) > viewDistance ||
-					   abs(static_cast<int>(std::get<1>(keyTuple)) - keyBase.y) > viewDistance ||
-					   abs(static_cast<int>(std::get<2>(keyTuple)) - keyBase.z) > viewDistance) {
+					if(abs(static_cast<int>(std::get<0>(keyTuple)) - chunkCoord.x) > viewDistance ||
+					   abs(static_cast<int>(std::get<1>(keyTuple)) - chunkCoord.y) > viewDistance ||
+					   abs(static_cast<int>(std::get<2>(keyTuple)) - chunkCoord.z) > viewDistance) {
 						auto &status = std::get<0>(it->second);
 						switch(status) {
 						case ChunkStatus::Generating:
@@ -55,18 +54,27 @@ void World::Update(DeltaTicks &) {
 							/* only operate on outer ring so at least one of x y z must have absolute value of d */
 							if(glm::abs(x) != d && glm::abs(y) != d && glm::abs(z) != d) { continue; }
 
-							auto key = keyBase + glm::ivec3(x, y, z);
-							auto keyTuple = std::make_tuple(key.x, key.y, key.z);
+							auto coord = chunkCoord + Point3(x, y, z);
+							ChunkKeyType coordTuple = std::make_tuple(static_cast<uint64_t>(coord.x),
+							                                          static_cast<uint64_t>(coord.y),
+							                                          static_cast<uint64_t>(coord.z));
 
-							if(chunks.With<bool>([&keyTuple] (ChunksType &chunks) {
-								return chunks.find(keyTuple) == chunks.end();
+							if(chunks.With<bool>([&coordTuple] (ChunksType &chunks) {
+								return chunks.find(coordTuple) == chunks.end();
 							})) {
-								chunks.With([&keyTuple] (ChunksType &chunks) {
-									chunks.emplace(keyTuple, std::make_tuple(ChunkStatus::Generating, 0));
+								chunks.With([&coordTuple] (ChunksType &chunks) {
+									chunks.emplace(coordTuple, std::make_tuple(ChunkStatus::Generating, 0));
 								});
-								jobM->Do([this, jobM, key, keyTuple, chunkSizeF] () {
-									auto dead = chunks.With<bool>([&keyTuple] (ChunksType &chunks) {
-										auto &status = std::get<0>(chunks.at(keyTuple));
+								jobM->Do([this, jobM, coord] () {
+									/* create tuple again inside of copying by binding to lambda
+									 * tuples are packed at compile-time anyway or something
+									 */
+									ChunkKeyType coordTuple = std::make_tuple(static_cast<uint64_t>(coord.x),
+																			  static_cast<uint64_t>(coord.y),
+																			  static_cast<uint64_t>(coord.z));
+
+									auto dead = chunks.With<bool>([&coordTuple] (ChunksType &chunks) {
+										auto &status = std::get<0>(chunks.at(coordTuple));
 										if(status == ChunkStatus::Dying) {
 											status = ChunkStatus::Dead;
 											return true;
@@ -74,8 +82,9 @@ void World::Update(DeltaTicks &) {
 									});
 									if(dead) { return; }
 
-									auto data = GenerateChunk(Point3(std::get<0>(keyTuple), std::get<1>(keyTuple), std::get<2>(keyTuple)));
-									auto pos = new PositionComponent(Point3(key) * chunkSizeF);
+									auto chunkSizeF = Point3(chunkSize);
+									auto data = GenerateChunk(coord);
+									auto pos = new PositionComponent(coord * chunkSizeF);
 									auto chunk = new Chunk(chunkSize.x, chunkSize.y, chunkSize.z, data);
 									auto bounds = new BoundingComponent(Point3(0.0f), blockSize * chunkSizeF);
 
@@ -90,13 +99,13 @@ void World::Update(DeltaTicks &) {
 										}
 									}
 
-									jobM->Do([this, keyTuple, pos, chunk, bounds] () {
+									jobM->Do([this, coordTuple, pos, chunk, bounds] () {
 										auto id = engine->AddObject();
 										engine->InsertComponent<PositionComponent>(id, pos);
 										engine->InsertComponent<ModelComponent>(id, chunk);
 										engine->InsertComponent<BoundingComponent>(id, bounds);
-										chunks.With([&keyTuple, id] (ChunksType &chunks) {
-											chunks.at(keyTuple) = std::make_tuple(ChunkStatus::Alive, id);
+										chunks.With([&coordTuple, id] (ChunksType &chunks) {
+											chunks.at(coordTuple) = std::make_tuple(ChunkStatus::Alive, id);
 										});
 									}, JobPriority::High, JobThread::Main);
 								}, JobPriority::Normal, JobThread::Worker);
@@ -115,51 +124,4 @@ World::~World() {
 			engine->RemoveObject(std::get<1>(it->second));
 		}
 	});
-}
-
-std::vector<bool> World::GenerateChunk(const Point3 &&p) const {
-	std::vector<bool> blocks;
-	blocks.resize(chunkSize.x * chunkSize.y * chunkSize.z);
-
-	Point3 actual = p * Point3(blockSize.x * chunkSize.x, blockSize.y * chunkSize.y, blockSize.z * chunkSize.z);
-
-	int i = 0;
-	for(float z = 0; z < blockSize.z * chunkSize.z; z += blockSize.z) {
-		for(float x = 0; x < blockSize.x * chunkSize.x; x += blockSize.x) {
-			for(float y = 0; y < blockSize.y * chunkSize.y; y += blockSize.y) {
-				blocks.at(i++) = GenerateBlock(actual + Point3(x, y, z));
-			}
-		}
-	}
-	return blocks;
-}
-
-bool World::GenerateBlock(const Point3 &&p) const {
-	/* periodic 'normal distribution' function */
-	auto fDensity = [] (const double x) {
-		return 1.0 - glm::abs(glm::sin(x));
-	};
-
-	const float scale1 = 17.0f;
-	const float scale2 = scale1 / 17.0f * 8.0f;
-	auto eval1 = noise.eval(p.x / scale1, p.y * 1.6f / scale1, p.z / scale1);
-	auto eval2 = noise.eval(p.x / scale2, p.y / scale2 / 3.0f, p.z / scale2 / 5.0f);
-
-	/* cave systems */
-	auto result1 = (
-		eval1 > (fDensity(p.x / scale1 / 32.0f) - 1) * 1.5f + 0.1f ||
-		eval1 > (fDensity(p.y / scale1 / 10.0f) - 1) * 1.5f + 0.1f ||
-		eval1 > (fDensity(p.z / scale1 / 32.0f) - 1) * 1.5f + 0.1f
-		);
-	/* cave system crevices */
-	auto result2 = (
-		eval1 > (fDensity(p.x / scale1 / 32.0f) - 1) * 1.5f + 0.1f ||
-		eval1 > (fDensity(p.y / scale1 / 10.0f + noise.eval(p.x / scale1 / 60.0f, p.z / scale1 / 60.0f) * 100.0f) - 1) * 1.5f + 0.1f ||
-		eval1 > (fDensity(p.z / scale1 / 32.0f) - 1) * 1.5f + 0.1f
-		);
-	/* crevices */
-	auto result3 = eval2 > -0.75;
-
-	/* AND results so there is only a block if no results dictate no block */
-	return result1 && result2 && result3;
 }
