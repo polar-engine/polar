7fb3969eb87135f25fc10dbe4db5922cd45579b7
Shockk <shokku.ra@gmail.com>
2015-10-24 02:44:00 +0100

    Write square waveshape; use 432 Hz for A

--- Sub-Terra/include/Oscillator.h
+++ Sub-Terra/include/Oscillator.h
@@ -2,31 +2,35 @@
 
 #include "WaveShape.h"
 
+#define LIGHTSPEED_MILES_PER_SECOND (186282.3970512)
+#define SCIENTIFIC_A (sqrt(LIGHTSPEED_MILES_PER_SECOND))
+#define MIDDLE_C (-9)
+#define NOTE_TO_FREQUENCY(X) (SCIENTIFIC_A * std::pow(std::pow(2.0, 1.0 / 12.0), X))
+
 class Oscillator {
+protected:
+	uint32_t phaseAccumulator = 0;
 public:
 	WaveShape waveShape;
 	double frequency = 1.0;
 	double amplitude = 1.0;
-	double speed = 1.0;
-	uint32_t phaseAccumulator = 0;
 
 	Oscillator() = default;
-	Oscillator(const WaveShape &waveShape) : waveShape(waveShape), frequency(waveShape.preferredFrequency) {}
+	Oscillator(const WaveShape &waveShape, const double frequency = NOTE_TO_FREQUENCY(MIDDLE_C + 12)) : waveShape(waveShape), frequency(frequency) {}
 
-	static constexpr uint32_t LowerMask(unsigned int n) {
+	static constexpr uint32_t LowerMask(const unsigned int n) {
 		return (n == 0)
 			? 0
 			: ((LowerMask(n - 1) << 1) | 1);
 	}
 
-	static constexpr uint32_t UpperMask(unsigned int n) {
+	static constexpr uint32_t UpperMask(const unsigned int n) {
 		return LowerMask(n) << (32 - n);
 	}
 
-	inline uint16_t Tick(const double sampleRate) {
-		double frequencyOut = frequency * speed;
-		uint32_t frequencyControlWord = static_cast<uint32_t>(frequencyOut * static_cast<double>(1ull << 32) / sampleRate);
+	inline int16_t Tick(const double sampleRate) {
+		uint32_t frequencyControlWord = static_cast<uint32_t>(frequency * static_cast<double>(1ull << 32) / sampleRate);
 		phaseAccumulator += frequencyControlWord;
-		return static_cast<uint32_t>(amplitude * waveShape.table[(phaseAccumulator & UpperMask(WaveShape::granularity)) >> 22]);
+		return static_cast<int16_t>(amplitude * waveShape.table[(phaseAccumulator & UpperMask(WaveShape::granularity)) >> 22]);
 	}
 };
--- Sub-Terra/include/WaveShape.h
+++ Sub-Terra/include/WaveShape.h
@@ -4,18 +4,25 @@
 
 struct WaveShape {
 public:
-	static const unsigned int granularity = 10;
-	static const uint16_t size = 1 << granularity;
-	boost::array<uint16_t, size> table;
-	double preferredFrequency;
+	static const unsigned int granularity = 12;
+	static const int16_t size = 1 << granularity;
+	boost::array<int16_t, size> table;
 };
 
 inline WaveShape MkSineWaveShape() {
 	WaveShape waveShape;
 	for(unsigned int i = 0; i < WaveShape::size; ++i) {
 		double sample = sin(i * 2.0 * 3.14159265358979 / WaveShape::size);
-		waveShape.table[i] = static_cast<uint16_t>((sample + 1) * 32767.0 - 32768.0);
+		waveShape.table[i] = static_cast<int16_t>((sample + 1) * 32767.0 - 32768.0);
 	}
-	waveShape.preferredFrequency = 261.625565; /* middle C */
+	return waveShape;
+}
+
+inline WaveShape MkSquareWaveShape() {
+	WaveShape waveShape;
+	for(unsigned int i = 0; i < WaveShape::size; ++i) {
+		double sample = i * 2 < WaveShape::size ? 1.0 : -1.0;
+		waveShape.table[i] = static_cast<int16_t>((sample + 1) * 32767.0 - 32768.0);
+	}
 	return waveShape;
 }
