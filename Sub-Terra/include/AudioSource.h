feca3444461d90124ba1f58cb0bbf213acd895c9
Shockk <shokku.ra@gmail.com>
2015-10-28 02:27:20 +0000

    Move AudioSource into own file

---
+++ Sub-Terra/include/AudioSource.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Component.h"
+#include "AudioAsset.h"
+
+struct LoopIn {
+	size_t value;
+};
+
+struct LoopOut {
+	size_t value;
+};
+
+class AudioSource : public Component {
+protected:
+	AudioAsset asset;
+	bool looping;
+	size_t loopIn;
+	size_t loopOut;
+	size_t currentSample = 0;
+public:
+	AudioSource(const AudioAsset &as, const bool looping = false) : asset(as), looping(looping), loopIn(0), loopOut(as.samples.size()) {}
+	AudioSource(const AudioAsset &as, const LoopIn  &in) : asset(as), looping(true), loopIn(in.value), loopOut(as.samples.size()) {}
+	AudioSource(const AudioAsset &as, const LoopOut &out) : asset(as), looping(true), loopIn(0), loopOut(out.value) {}
+	AudioSource(const AudioAsset &as, const LoopIn  &in, const LoopOut &out) : asset(as), looping(true), loopIn(in.value), loopOut(out.value) {}
+
+	bool Tick(int16_t &left, int16_t &right) {
+		if(looping) {
+			if(currentSample == loopOut || currentSample == asset.samples.size()) { currentSample = loopIn; }
+		} else if(currentSample == asset.samples.size()) { return false; }
+
+		if(asset.stereo) {
+			left += asset.samples[currentSample];
+			right += asset.samples[currentSample + 1];
+			currentSample += 2;
+		} else {
+			left += asset.samples[currentSample];
+			right += asset.samples[currentSample];
+			++currentSample;
+		}
+		return true;
+	}
+};
