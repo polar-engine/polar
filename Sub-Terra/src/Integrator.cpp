c279b53338a0c13d2a8bd0990b2ca4f99d4e7627
Shockk <shokku.ra@gmail.com>
2015-11-13 04:15:41 +0000

    Set integrator microseconds value at end of tick

--- Sub-Terra/src/Integrator.cpp
+++ Sub-Terra/src/Integrator.cpp
@@ -6,12 +6,13 @@
 void Integrator::Update(DeltaTicks &dt) {
 	accumulator += dt;
 	if(accumulator.Seconds() > 1.0f) { accumulator.SetSeconds(1.0f); }
-	alphaMicroseconds = static_cast<uint32_t>(accumulator.Seconds() * 1000000.0f);
 
 	while(accumulator >= timestep) {
 		Tick(timestep.Seconds());
 		accumulator -= timestep;
 	}
+
+	alphaMicroseconds = static_cast<uint32_t>(accumulator.Seconds() * 1000000.0f);
 }
 
 void Integrator::Tick(DeltaTicks::seconds_type seconds) {
