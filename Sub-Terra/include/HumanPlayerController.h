86b83cedb4da3ce8c3ca2398c1e608eb194127f6
David Farrell <shokku.ra@gmail.com>
2017-04-02 16:57:23 +0100

    Implement basic perlin noise raycaster

--- Sub-Terra/include/HumanPlayerController.h
+++ Sub-Terra/include/HumanPlayerController.h
@@ -7,6 +7,7 @@ private:
 	Point2 orientRot;
 	long double accum = 0.0;
 	float velocity = 10.0f;
+	bool moveForward = false, moveBackward = false, moveLeft = false, moveRight = false;
 protected:
 	virtual void Init() override;
 	virtual void Update(DeltaTicks &) override;
--- Sub-Terra/src/SubTerra.cpp
+++ Sub-Terra/src/SubTerra.cpp
@@ -28,7 +28,7 @@ void SubTerra::Run(const std::vector<std::string> &args) {
 		st.AddSystem<Integrator>();
 		st.AddSystem<Tweener<float>>();
 		st.AddSystem<AudioManager>();
-		st.AddSystem<GL32Renderer, const boost::container::vector<std::string> &>({"main", "unified", "fxaa", "gaussian"});
+		st.AddSystem<GL32Renderer, const boost::container::vector<std::string> &>({ "main", "perlin"});
 
 		auto assetM = engine->GetSystem<AssetManager>().lock();
 		assetM->Get<AudioAsset>("beep1");
