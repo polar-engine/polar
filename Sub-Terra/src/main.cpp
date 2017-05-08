379eb7e1c7b803dbe7d0e284e42f70c19c1c547e
David Farrell <shokku.ra@gmail.com>
2017-05-08 15:54:19 +0100

    Handle args in engine, not in main

--- Sub-Terra/src/main.cpp
+++ Sub-Terra/src/main.cpp
@@ -13,34 +13,10 @@ int main(int argc, char **argv) {
 #endif
 	std::vector<std::string> args;
 	for(int i = 0; i < argc; ++i) {
-		args.emplace_back(std::string(argv[i]));
+		args.emplace_back(argv[i]);
 	}
 
-	for(auto &arg : args) {
-		if(arg == "-console") {
-#if defined(_WIN32)
-			AllocConsole();
-			freopen("CONIN$", "r", stdin);
-			freopen("CONOUT$", "w", stdout);
-			freopen("CONOUT$", "w", stderr);
-			std::wcout.clear();
-			std::cout.clear();
-			std::wcerr.clear();
-			std::cerr.clear();
-			std::wcin.clear();
-			std::cin.clear();
-			break;
-#endif
-		} else if(arg == "-trace") {
-			DebugManager()->priority = DebugPriority::Trace;
-		} else if(arg == "-debug") {
-			DebugManager()->priority = DebugPriority::Debug;
-		} else if(arg == "-verbose") {
-			DebugManager()->priority = DebugPriority::Verbose;
-		}
-	}
-
-	Polar engine;
+	Polar engine(args);
 	auto app = Freefall(engine);
 	return 0;
 }
