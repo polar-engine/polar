3ac56ad16387ff7fb475d86f21d7034fb583f2d4
David Farrell <shokku.ra@gmail.com>
2017-05-16 01:22:23 +0100

    Print all debug messages to log.txt

--- src/DebugManager.cpp
+++ src/DebugManager.cpp
@@ -1,4 +1,5 @@
 #include "DebugManager.h"
+#include "FileSystem.h"
 #include "sdl.h"
 
 std::shared_ptr<DebugManagerClass> DebugManagerClass::instance;
@@ -6,3 +7,8 @@ std::shared_ptr<DebugManagerClass> DebugManagerClass::instance;
 void DebugManagerClass::MsgBox(std::string title, std::string msg) {
 	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title.data(), msg.data(), NULL);
 }
+
+
+DebugManagerClass::DebugManagerClass(DebugPriority priority) :
+	priority(priority),
+	file(FileSystem::GetAppDir() + "/log.txt", std::ios::out | std::ios::binary | std::ios::trunc) {}
