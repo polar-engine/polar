921133134eb543db304fb1ecc21d06758547a8c3
David Farrell <shokku.ra@gmail.com>
2017-08-10 06:55:53 +0100

    Implement async partial asset loading

--- Sub-Terra/include/AssetManager.h
+++ Sub-Terra/include/AssetManager.h
@@ -5,9 +5,15 @@
 #include "FileSystem.h"
 #include "Serializer.h"
 
+struct PartialAsset {
+	bool done = false;
+	std::string contents;
+	std::shared_ptr<Asset> asset;
+};
+
 class AssetManager : public System {
 private:
-	std::unordered_map<std::string, std::shared_ptr<Asset>> cache;
+	std::unordered_map<std::string, PartialAsset> partials;
 public:
 	static std::string GetAssetsDir() {
 #if defined(_WIN32)
@@ -42,18 +48,65 @@ public:
 	static bool IsSupported() { return true; }
 	AssetManager(Polar *engine) : System(engine) {}
 
+	void Update(DeltaTicks &) override final {
+		for(auto &pair : partials) {
+			auto &partial = pair.second;
+			if(!partial.done) {
+				auto &path = pair.first;
+				bool eof = false;
+				partial.contents += FileSystem::Read(path, partial.contents.size(), 262144, &eof);
+				if(eof) {
+					partial.done = true;
+					DebugManager()->Verbose("async loaded asset ", path);
+				}
+			}
+		}
+	}
+
+	template<typename T> void Request(const std::string name) {
+		static_assert(std::is_base_of<Asset, T>::value, "AssetManager::GetString requires typename of type Asset");
+		auto path = GetPath<T>(name);
+		if(partials.find(path) == partials.end()) {
+			partials.emplace(path, PartialAsset());
+			DebugManager()->Verbose("async loading asset ", path);
+		}
+	}
+
+	template<typename T> PartialAsset & ForcePartial(const std::string name) {
+		static_assert(std::is_base_of<Asset, T>::value, "AssetManager::GetString requires typename of type Asset");
+		auto path = GetPath<T>(name);
+		if(partials.find(path) == partials.end()) {
+			DebugManager()->Verbose("loading asset ", path);
+			partials.emplace(path, PartialAsset());
+		}
+		auto &partial = partials[path];
+		if(!partial.done) {
+			auto test = partials[path];
+			partial.contents += FileSystem::Read(path, partial.contents.size());
+			partial.done = true;
+			DebugManager()->Verbose("loaded asset ", path);
+		}
+		return partial;
+	}
+
 	template<typename T, typename ...Ts> std::shared_ptr<T> Get(const std::string name, Ts && ...args) {
 		static_assert(std::is_base_of<Asset, T>::value, "AssetManager::Get requires typename of type Asset");
-		auto path = GetPath<T>(name);
-		if(cache.find(path) == cache.end()) {
-			DebugManager()->Verbose("loading asset ", AssetName<T>(), "/", name);
+		auto &partial = ForcePartial<T>(name);
+		if(!partial.asset) {
+			DebugManager()->Verbose("deserializing asset ", AssetName<T>(), '/', name);
+
+			// deserialize raw asset data to sub-class of Asset
 			auto asset = std::make_shared<T>(std::forward<Ts>(args)...);
-			std::istringstream ss(FileSystem::Read(path));
+			std::istringstream ss(partial.contents);
 			Deserializer deserializer(ss);
 			deserializer >> *asset;
-			cache.emplace(path, std::static_pointer_cast<Asset>(asset));
-			DebugManager()->Verbose("loaded asset");
+			partial.asset = std::static_pointer_cast<Asset>(asset);
+
+			// free up raw asset data
+			std::string().swap(partial.contents);
+
+			DebugManager()->Verbose("deserialized asset ", AssetName<T>(), '/', name);
 		}
-		return std::static_pointer_cast<T>(cache.at(path));
+		return std::static_pointer_cast<T>(partial.asset);
 	}
 };
