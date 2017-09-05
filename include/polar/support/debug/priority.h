6ad3be1bd94a4d3e1383362de4cda406434a31a3
David Farrell <shokku.ra@gmail.com>
2017-09-05 08:47:26 +0100

    Rewrite gitignore file, include missing files

---
+++ include/polar/support/debug/priority.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace polar { namespace support { namespace debug {
+	enum class priority : uint_fast8_t {
+		trace,
+		debug,
+		verbose,
+		info,
+		notice,
+		warning,
+		error,
+		critical,
+		fatal
+	};
+} } }
