28406100552d87f365cc7a172bc59962c6732b4d
David Farrell <shokku.ra@gmail.com>
2017-09-22 03:06:45 +0100

    Move code out of polar engine

--- include/polar/fs/local.h
+++ include/polar/fs/local.h
@@ -70,20 +70,20 @@ namespace polar { namespace fs {
 	#endif
 		}
 
-		static std::string savedgamesdir() {
+		static std::string savedgamesdir(std::string name) {
 	#if defined(_WIN32)
 			char szPath[MAX_PATH];
 			auto result = SHGetFolderPathA(NULL, CSIDL_PERSONAL | CSIDL_FLAG_CREATE, NULL, 0, szPath);
 			if(FAILED(result)) { debugmanager()->fatal("CSIDL_PERSONAL: failed to retrieve path"); }
 			PathAppendA(szPath, "My Games");
-			PathAppendA(szPath, "Freefall");
+			PathAppendA(szPath, name.data());
 			return std::string(szPath);
 	#elif defined(__linux__)
 			struct passwd *pwd = getpwuid(getuid());
-			return std::string(pwd->pw_dir) + "/mygames/Freefall";
+			return std::string(pwd->pw_dir) + "/mygames/" + name;
 	#elif defined(__APPLE__)
 			struct passwd *pwd = getpwuid(getuid());
-			return std::string(pwd->pw_dir) + "/Documents/My Games/Freefall";
+			return std::string(pwd->pw_dir) + "/Documents/My Games/" + name;
 	#else
 			debugmanager()->fatal("polar::fs::local::savedgamesdir: not implemented");
 			return "";
