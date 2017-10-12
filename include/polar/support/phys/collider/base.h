42e55eb193650ec312b3a9b7bacc1411b19ac3b0
David Farrell <shokku.ra@gmail.com>
2017-10-12 14:52:40 +0100

    Add phys stub classes

---
+++ include/polar/support/phys/collider/base.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace polar {
+namespace support {
+	namespace phys {
+		namespace collider {
+			class base {};
+		}
+	}
+}
+}
---
+++ include/polar/support/phys/collider/static.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <polar/support/phys/collider/base.h>
+
+namespace polar {
+namespace support {
+	namespace phys {
+		namespace collider {
+			class static_ : public base {};
+		}
+	}
+}
+}
