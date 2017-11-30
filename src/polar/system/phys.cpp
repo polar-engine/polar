a24da3cd9cb1c667b307af991b50873f2b398db9
David Farrell <shokku.ra@gmail.com>
2017-11-30 19:35:44 +0000

    Add box-box collision resolver

--- src/polar/system/phys.cpp
+++ src/polar/system/phys.cpp
@@ -1,4 +1,7 @@
 #include <polar/component/phys.h>
+#include <polar/component/position.h>
+#include <polar/component/scale.h>
+#include <polar/support/phys/detector/box.h>
 #include <polar/system/event.h>
 #include <polar/system/phys.h>
 
@@ -37,5 +40,34 @@ namespace polar::system {
             }
 		};
 		dtors.emplace_back(eventM->listenfor("integrator", "ticked", f));
+
+		add<support::phys::detector::box, support::phys::detector::box>(
+		    [](core::polar *engine, auto a, auto b) {
+			    Point3 originA, originB;
+			    if(auto p = engine->get<component::position>(a.id)) {
+				    originA += p->pos.get();
+			    }
+			    if(auto p = engine->get<component::position>(b.id)) {
+				    originB += p->pos.get();
+			    }
+
+			    Point3 scaleA = a.detector->size;
+			    Point3 scaleB = b.detector->size;
+			    if(auto s = engine->get<component::scale>(a.id)) {
+				    scaleA *= s->sc.get();
+			    }
+			    if(auto s = engine->get<component::scale>(b.id)) {
+				    scaleB *= s->sc.get();
+			    }
+
+			    auto minA = originA - scaleA;
+			    auto maxA = originA + scaleA;
+			    auto minB = originB - scaleB;
+			    auto maxB = originB + scaleB;
+
+			    return minA.x <= maxB.x && maxA.x >= minB.x &&
+			           minA.y <= maxB.y && maxA.y >= minB.y &&
+			           minA.z <= maxB.z && maxA.z >= minB.z;
+		    });
 	}
 } // namespace polar::system
