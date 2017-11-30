c5a4335096211db6c1a6e992a6505570772bc106
David Farrell <shokku.ra@gmail.com>
2017-11-30 23:44:04 +0000

    Implement stat/rigid collision responders

--- include/polar/support/phys/responder/base.h
+++ include/polar/support/phys/responder/base.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <polar/core/polar.h>
+
 namespace polar::support::phys::responder {
 	class base {
 	  public:
 		virtual ~base() = default;
+		virtual void respond(core::polar *, IDType, DeltaTicks) {}
 	};
 } // namespace polar::support::phys::responder
--- include/polar/support/phys/responder/rigid.h
+++ include/polar/support/phys/responder/rigid.h
@@ -3,5 +3,13 @@
 #include <polar/support/phys/responder/base.h>
 
 namespace polar::support::phys::responder {
-	class rigid : public base {};
+	class rigid : public base {
+	  public:
+		void respond(core::polar *engine, IDType id, DeltaTicks) override {
+			if(auto p = engine->get<component::position>(id)) {
+				p->pos.revert();
+				p->pos.derivative()->x *= -1;
+			}
+		}
+	};
 } // namespace polar::support::phys::responder
