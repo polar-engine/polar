d90ab4a80ea44875c72db7a51be23d0e265faeaf
David Farrell <shokku.ra@gmail.com>
2018-02-25 23:46:30 +0000

    menu: Add mouse selection of menu items

--- src/polar/system/menu.cpp
+++ src/polar/system/menu.cpp
@@ -7,12 +7,14 @@
 #include <polar/component/sprite/slider.h>
 #include <polar/component/text.h>
 #include <polar/support/action/menu.h>
+#include <polar/support/action/mouse.h>
 #include <polar/support/audio/sourcetype.h>
 #include <polar/support/input/key.h>
 #include <polar/support/ui/control.h>
 #include <polar/system/action.h>
 #include <polar/system/asset.h>
 #include <polar/system/menu.h>
+#include <polar/system/renderer/base.h>
 #include <polar/system/tweener.h>
 
 namespace polar::system {
@@ -80,32 +82,45 @@ namespace polar::system {
 			render_all();
 		}
 		if(down != 0) {
-			auto sz = current_size();
-			auto previous = current;
-			current += down;
-			if(current < 0) {
-				current += sz;
-			} else {
-				current %= sz;
-			}
-			render(previous, true);
-			playBeep = true;
+			navigate_to(current + down);
 		}
 
 		if(playBeep) {
-			auto assetM = engine->get<asset>().lock();
-			IDType soundID;
-			soundDtors[soundIndex++] = engine->add(soundID);
-			soundIndex %= soundDtors.size();
-			engine->add<component::audiosource>(
-			    soundID, assetM->get<polar::asset::audio>("menu1"),
-			    support::audio::sourcetype::effect);
+			beep();
 		}
 	}
 
+	void menu::navigate_to(int to) {
+		if(to == current) { return; }
+		debugmanager()->debug(to, ' ', current);
+
+		auto sz = current_size();
+		auto previous = current;
+		current = to;
+		if(current < 0) {
+			current += sz;
+		} else {
+			current %= sz;
+		}
+		render(previous, true);
+		beep();
+	}
+
+	void menu::beep() {
+		auto assetM = engine->get<asset>().lock();
+		IDType soundID;
+		soundDtors[soundIndex++] = engine->add(soundID);
+		soundIndex %= soundDtors.size();
+		engine->add<component::audiosource>(
+		    soundID, assetM->get<polar::asset::audio>("menu1"),
+		    support::audio::sourcetype::effect);
+	}
+
 	void menu::render(size_t i, bool replace) {
-		auto &item = current_at(i);
 		auto size = current_size();
+		if(i < 0 || i >= size) { return; }
+
+		auto &item = current_at(i);
 
 		IDType id;
 		if(replace) {
@@ -113,11 +128,6 @@ namespace polar::system {
 		} else {
 			itemDtors.emplace_back(engine->add(id));
 		}
-
-		// max 8 items on screen at max scale of uiBase
-		const Decimal uiHeight     = 8 * uiBase;
-		const Decimal uiTextHeight = 160;
-		const Decimal uiTextWidth  = 550;
 
 		Decimal scale =
 		    glm::min(Decimal(uiHeight) / Decimal(size), Decimal(uiScale));
@@ -141,9 +151,28 @@ namespace polar::system {
 		}
 	}
 
+	void menu::on_cursor(int y) {
+		auto size = current_size();
+
+		auto scale = glm::min(uiHeight / Decimal(size), uiScale);
+		auto spacing = uiTextHeight * scale;
+
+		auto height = engine->get<renderer::base>().lock()->getheight();
+		auto origin = height - y;
+
+		/* origin = 50 + spacing * (size - i - 1);
+		 * spacing * (size - i - 1) = origin - 50
+		 * size - i - 1 = (origin - 50) / spacing
+		 * i = size - (origin - 50) / spacing - 1
+		 */
+		auto i = int(glm::floor(size - (origin - 50) / spacing));
+		if(i >= 0 && i < size) { navigate_to(i); }
+	}
+
 	void menu::init() {
 		using lifetime = support::action::lifetime;
 		namespace a = support::action::menu;
+		namespace mse = support::action::mouse;
 
 		auto assetM = engine->get<asset>().lock();
 		auto act    = engine->get<action>().lock();
@@ -160,6 +189,10 @@ namespace polar::system {
 		keep(act->bind<a::forward>(lifetime::on, [this] { activate();      }));
 		keep(act->bind<a::back   >(lifetime::on, [this] {
 			navigate(0, -1, true);
+		}));
+
+		keep(act->bind<mse::position_y>([this](Decimal y) {
+			if(y != 0) { on_cursor(y); }
 		}));
 
 		keep(tw->tween(0.0f, 1.0f, 0.25, true, [this](auto, const float &x) {
