02bcd5ba54cd656316f8cfad3034aca4e51efb65
David Farrell <shokku.ra@gmail.com>
2018-03-06 06:01:55 +0000

    vr: Add controller input actions

---
+++ include/polar/support/action/vr.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <polar/system/action.h>
+
+namespace polar::support::action::vr {
+	enum class type {
+		left_hand,
+		right_hand,
+		any
+	};
+
+	template<type Ty> struct app_menu : system::action::digital {};
+	template<type Ty> struct a        : system::action::digital {};
+}
