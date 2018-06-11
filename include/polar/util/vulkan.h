d50a42020c2c5c2ffd010d4e9582859b37082063
David Farrell <shokku.ra@gmail.com>
2018-06-11 22:31:35 +0100

    Vk: Create swapchain

--- include/polar/util/vulkan.h
+++ include/polar/util/vulkan.h
@@ -4,10 +4,25 @@
 #include <vulkan/vulkan.h>
 
 #define POLAR_VK_FNS \
-	POLAR_VK_GLOBAL_FN(vkCreateInstance)                  \
-	POLAR_VK_INSTANCE_FN(vkDestroyInstance)               \
-	POLAR_VK_INSTANCE_FN(vkCreateDebugReportCallbackEXT)  \
-	POLAR_VK_INSTANCE_FN(vkDestroyDebugReportCallbackEXT)
+	POLAR_VK_DEVICE_FN(vkCreateSwapchainKHR)                        \
+	POLAR_VK_DEVICE_FN(vkDestroyDevice)                             \
+	POLAR_VK_DEVICE_FN(vkDestroySwapchainKHR)                       \
+	POLAR_VK_DEVICE_FN(vkGetDeviceQueue)                            \
+	POLAR_VK_GLOBAL_FN(vkCreateInstance)                            \
+	POLAR_VK_INSTANCE_FN(vkCreateDebugReportCallbackEXT)            \
+	POLAR_VK_INSTANCE_FN(vkCreateDevice)                            \
+	POLAR_VK_INSTANCE_FN(vkDestroyDebugReportCallbackEXT)           \
+	POLAR_VK_INSTANCE_FN(vkDestroyInstance)                         \
+	POLAR_VK_INSTANCE_FN(vkDestroySurfaceKHR)                       \
+	POLAR_VK_INSTANCE_FN(vkEnumerateDeviceExtensionProperties)      \
+	POLAR_VK_INSTANCE_FN(vkEnumeratePhysicalDevices)                \
+	POLAR_VK_INSTANCE_FN(vkGetDeviceProcAddr)                       \
+	POLAR_VK_INSTANCE_FN(vkGetPhysicalDeviceProperties)             \
+	POLAR_VK_INSTANCE_FN(vkGetPhysicalDeviceQueueFamilyProperties)  \
+	POLAR_VK_INSTANCE_FN(vkGetPhysicalDeviceSurfaceCapabilitiesKHR) \
+	POLAR_VK_INSTANCE_FN(vkGetPhysicalDeviceSurfaceFormatsKHR)      \
+	POLAR_VK_INSTANCE_FN(vkGetPhysicalDeviceSurfacePresentModesKHR) \
+	POLAR_VK_INSTANCE_FN(vkGetPhysicalDeviceSurfaceSupportKHR)
 
 #define POLAR_VK_DEVICE_FN(NAME)   static PFN_##NAME NAME = nullptr;
 #define POLAR_VK_GLOBAL_FN(NAME)   static PFN_##NAME NAME = nullptr;
