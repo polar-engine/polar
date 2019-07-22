d6f83ec60e067f75991b6f34ebb5be2388b2899b
David Farrell <shokku.ra@gmail.com>
2019-07-22 20:05:18 +0100

    Add explicit Quat overloads

--- include/polar/component/orientation.h
+++ include/polar/component/orientation.h
@@ -6,7 +6,10 @@ namespace polar::component {
 	class orientation : public base {
 	  public:
 		Quat orient{1, 0, 0, 0};
+
 		orientation() {}
+		orientation(const Quat &orient) : orient(orient) {}
+		orientation(const Quat &&orient) : orient(orient) {}
 		orientation(const Point3 &euler) : orient(euler) {}
 		orientation(const Point3 &&euler) : orient(euler) {}
 	};
