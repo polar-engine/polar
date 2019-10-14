eaa3fe50b503a5474dcb5b99b332d2cf836401cc
David Farrell <shokku.ra@gmail.com>
2019-10-14 19:54:39 +0100

    Write very basic kludge API

---
+++ include/polar/api/kludge.h
@@ -0,0 +1,292 @@
+#pragma once
+
+#include <variant>
+
+namespace polar::api {
+	class kludge {
+	  public:
+		enum class token_type {
+			equals,
+			accessor,
+			number,
+			identifier
+		};
+
+		class token {
+		  public:
+			using value_type = std::variant<std::monostate, Decimal, std::string>;
+		  protected:
+			token_type _type;
+			value_type _value;
+
+			token(token_type type, value_type value = {}) : _type(type), _value(value) {}
+		  public:
+			static token equals() {
+				return token{token_type::equals};
+			}
+
+			static token accessor() {
+				return token{token_type::accessor};
+			}
+
+			static token number(Decimal x) {
+				return token{token_type::number, x};
+			}
+
+			static token identifier(std::string x) {
+				return token{token_type::identifier, x};
+			}
+
+			auto type() const {
+				return _type;
+			}
+
+			template<typename T> T get() const {
+				return std::get<T>(_value);
+			}
+
+			friend std::ostream & operator<<(std::ostream &os, const token &t) {
+				switch(t.type()) {
+				case token_type::equals:
+					return os << "token::equals";
+				case token_type::accessor:
+					return os << "token::accessor";
+				case token_type::number:
+					return os << "token::number{" << t.get<Decimal>() << '}';
+				case token_type::identifier:
+					return os << "token::identifier{\"" << t.get<std::string>() << "\"}";
+				default:
+					return os << "token::invalid";
+				}
+			}
+		};
+
+		enum class expr_type {
+			number,
+			identifier
+		};
+
+		class expr {
+		  public:
+			using value_type = std::variant<std::monostate, Decimal, std::string>;
+		  protected:
+			expr_type _type;
+			std::vector<expr> _operands;
+			value_type _value;
+
+			expr(expr_type type, std::vector<expr> operands, value_type value) : _type(type), _operands(operands), _value(value) {}
+		  public:
+			static expr number(Decimal x) {
+				return expr{expr_type::number, {}, x};
+			}
+
+			static expr identifier(std::string x) {
+				return expr{expr_type::identifier, {}, x};
+			}
+
+			auto type() const {
+				return _type;
+			}
+
+			template<typename T> T get() const {
+				return std::get<T>(_value);
+			}
+		};
+	  protected:
+		core::polar *engine = nullptr;
+	  public:
+		kludge(core::polar *engine) : engine(engine) {}
+
+		std::pair<std::optional<char>, std::string_view>
+		lex_char(std::string_view str) const {
+			if(str.empty()) {
+				return {{}, str};
+			} else {
+				char c = str[0];
+				str.remove_prefix(1);
+				return {{c}, str};
+			}
+		}
+
+		std::string_view lex_whitespace(std::string_view str) const {
+			for(;;) {
+				auto [c, s] = lex_char(str);
+				if(c && *c == ' ') {
+					str = s;
+				} else {
+					break;
+				}
+			}
+			return str;
+		}
+
+		std::pair<std::optional<uint_fast8_t>, std::string_view>
+		lex_digit(std::string_view str) const {
+			auto [c, s] = lex_char(str);
+			if(c && *c >= '0' && *c <= '9') {
+				return {{*c - '0'}, s};
+			} else {
+				return {{}, str};
+			}
+		}
+
+		std::pair<std::optional<char>, std::string_view>
+		lex_identifier_char_first(std::string_view str) const {
+			auto [c, s] = lex_char(str);
+			if(c && *c >= 'a' && *c <= 'z') {
+				return {c, s};
+			} else {
+				return {{}, str};
+			}
+		}
+
+		std::pair<std::optional<char>, std::string_view>
+		lex_identifier_char(std::string_view str) const {
+			auto [c, s] = lex_char(str);
+			if(c && (*c >= 'a' && *c <= 'z' || *c >= '0' && *c <= '9')) {
+				return {c, s};
+			} else {
+				return {{}, str};
+			}
+		}
+
+		std::pair<std::optional<token>, std::string_view>
+		lex_number(std::string_view str) const {
+			str = lex_whitespace(str);
+
+			Decimal r = 0;
+
+			auto [d, s] = lex_digit(str);
+			if(d) {
+				r = *d;
+				str = s;
+			} else {
+				return {{}, str};
+			}
+
+			for(;;) {
+				auto [d, s] = lex_digit(str);
+				if(d) {
+					r = r * 10 + *d;
+					str = s;
+				} else {
+					break;
+				}
+			}
+
+			auto t = token::number(r);
+			return {{t}, str};
+		}
+
+		std::pair<std::optional<token>, std::string_view>
+		lex_identifier(std::string_view str) const {
+			str = lex_whitespace(str);
+
+			std::string r;
+
+			auto [c, s] = lex_identifier_char_first(str);
+			if(c) {
+				r.push_back(*c);
+				str = s;
+			} else {
+				return {{}, str};
+			}
+
+			for(;;) {
+				auto [c, s] = lex_identifier_char(str);
+				if(c) {
+					r.push_back(*c);
+					str = s;
+				} else {
+					break;
+				}
+			}
+
+			auto t = token::identifier(std::string{r});
+			return {{t}, str};
+		}
+
+		std::pair<std::optional<token>, std::string_view>
+		lex_one(std::string_view str) const {
+			str = lex_whitespace(str);
+
+			{
+				auto [c, s] = lex_char(str);
+				if(c) {
+					switch(*c) {
+					case '=':
+						return {token::equals(), s};
+					case '.':
+						return {token::accessor(), s};
+					}
+				}
+			}
+			{
+				auto [t, s] = lex_number(str);
+				if(t) {
+					return {t, s};
+				}
+			}
+			return lex_identifier(str);
+		}
+
+		auto lex(std::string_view str) const {
+			std::vector<token> tokens;
+
+			while(!str.empty()) {
+				auto [t, s] = lex_one(str);
+				if(t) {
+					tokens.emplace_back(*t);
+					str = s;
+				} else {
+					break;
+				}
+			}
+
+			return tokens;
+		}
+
+		auto parse(std::vector<token> tokens) const {
+			std::vector<expr> exprs;
+
+			for(auto &t : tokens) {
+				switch(t.type()) {
+				case token_type::identifier:
+					exprs.emplace_back(expr::identifier(t.get<std::string>()));
+					break;
+				}
+			}
+
+			return exprs;
+		}
+
+		auto parse(std::string_view str) const {
+			return parse(lex(str));
+		}
+
+		bool exec(std::vector<expr> exprs) {
+			bool ret = true;
+
+			for(auto &e : exprs) {
+				switch(e.type()) {
+				case expr_type::identifier:
+					if(e.get<std::string>() == "quit") {
+						engine->quit();
+					} else {
+						ret = false;
+					}
+					break;
+				default:
+					ret = false;
+					break;
+				}
+			}
+
+			return ret;
+		}
+
+		bool exec(std::string_view str) {
+			return exec(parse(str));
+		}
+	};
+} // namespace polar::api
--- include/polar/system/console.h
+++ include/polar/system/console.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <polar/api/kludge.h>
 #include <polar/asset/font.h>
 #include <polar/component/color.h>
 #include <polar/component/screenposition.h>
@@ -11,44 +12,71 @@ namespace polar::system {
 		using key      = support::input::key;
 		using lifetime = support::action::lifetime;
 
-		std::ostringstream buffer;
+		std::string buffer;
 		core::ref bufferDtor;
 		IDType bufferID;
 
-		void process(key k) {
+		void handle(key k) {
 			switch(k) {
-			case key::A:     buffer << 'a'; break;
-			case key::B:     buffer << 'b'; break;
-			case key::C:     buffer << 'c'; break;
-			case key::D:     buffer << 'd'; break;
-			case key::E:     buffer << 'e'; break;
-			case key::F:     buffer << 'f'; break;
-			case key::G:     buffer << 'g'; break;
-			case key::H:     buffer << 'h'; break;
-			case key::I:     buffer << 'i'; break;
-			case key::J:     buffer << 'j'; break;
-			case key::K:     buffer << 'k'; break;
-			case key::L:     buffer << 'l'; break;
-			case key::M:     buffer << 'm'; break;
-			case key::N:     buffer << 'n'; break;
-			case key::O:     buffer << 'o'; break;
-			case key::P:     buffer << 'p'; break;
-			case key::Q:     buffer << 'q'; break;
-			case key::R:     buffer << 'r'; break;
-			case key::S:     buffer << 's'; break;
-			case key::T:     buffer << 't'; break;
-			case key::U:     buffer << 'u'; break;
-			case key::V:     buffer << 'v'; break;
-			case key::W:     buffer << 'w'; break;
-			case key::X:     buffer << 'x'; break;
-			case key::Y:     buffer << 'y'; break;
-			case key::Z:     buffer << 'z'; break;
-			case key::Space: buffer << ' '; break;
+			case key::A:      buffer.push_back('a'); break;
+			case key::B:      buffer.push_back('b'); break;
+			case key::C:      buffer.push_back('c'); break;
+			case key::D:      buffer.push_back('d'); break;
+			case key::E:      buffer.push_back('e'); break;
+			case key::F:      buffer.push_back('f'); break;
+			case key::G:      buffer.push_back('g'); break;
+			case key::H:      buffer.push_back('h'); break;
+			case key::I:      buffer.push_back('i'); break;
+			case key::J:      buffer.push_back('j'); break;
+			case key::K:      buffer.push_back('k'); break;
+			case key::L:      buffer.push_back('l'); break;
+			case key::M:      buffer.push_back('m'); break;
+			case key::N:      buffer.push_back('n'); break;
+			case key::O:      buffer.push_back('o'); break;
+			case key::P:      buffer.push_back('p'); break;
+			case key::Q:      buffer.push_back('q'); break;
+			case key::R:      buffer.push_back('r'); break;
+			case key::S:      buffer.push_back('s'); break;
+			case key::T:      buffer.push_back('t'); break;
+			case key::U:      buffer.push_back('u'); break;
+			case key::V:      buffer.push_back('v'); break;
+			case key::W:      buffer.push_back('w'); break;
+			case key::X:      buffer.push_back('x'); break;
+			case key::Y:      buffer.push_back('y'); break;
+			case key::Z:      buffer.push_back('z'); break;
+			case key::Num1:   buffer.push_back('1'); break;
+			case key::Num2:   buffer.push_back('2'); break;
+			case key::Num3:   buffer.push_back('3'); break;
+			case key::Num4:   buffer.push_back('4'); break;
+			case key::Num5:   buffer.push_back('5'); break;
+			case key::Num6:   buffer.push_back('6'); break;
+			case key::Num7:   buffer.push_back('7'); break;
+			case key::Num8:   buffer.push_back('8'); break;
+			case key::Num9:   buffer.push_back('9'); break;
+			case key::Num0:   buffer.push_back('0'); break;
+			case key::Hyphen: buffer.push_back('-'); break;
+			case key::Equals: buffer.push_back('='); break;
+			case key::Comma:  buffer.push_back(','); break;
+			case key::Period: buffer.push_back('.'); break;
+			case key::Space:  buffer.push_back(' '); break;
 			case key::Enter:
+				process(buffer);
 				engine->transition = "back";
+				break;
+			case key::Backspace:
+				if(!buffer.empty()) { buffer.pop_back(); }
 				break;
 			}
 			render();
+		}
+
+		void process(std::string line) {
+			api::kludge kl{engine};
+			if(!kl.exec(line)) {
+				debugmanager()->critical("kludge exec failed");
+			}
+
+			debugmanager()->verbose(kl.lex(line));
 		}
 
 		void render() {
@@ -57,7 +85,7 @@ namespace polar::system {
 			auto assetM = engine->get<asset>().lock();
 			auto font   = assetM->get<polar::asset::font>("nasalization-rg");
 
-			engine->add<component::text>(bufferID, font, buffer.str());
+			engine->add<component::text>(bufferID, font, buffer);
 			engine->add<component::screenposition>(bufferID, Point2(5, 5), support::ui::origin::topleft);
 			engine->add<component::color>(bufferID, Point4(1, 1, 1, 0.8));
 			engine->add<component::scale>(bufferID, Point3(0.1));
@@ -71,63 +99,93 @@ namespace polar::system {
 				engine->transition = "back";
 			}, -1, false));
 
-			keep(act->bind<kb::key<key::A    >>(lifetime::on, [this](auto) { process(key::A);     }, -100, false));
-			keep(act->bind<kb::key<key::B    >>(lifetime::on, [this](auto) { process(key::B);     }, -100, false));
-			keep(act->bind<kb::key<key::C    >>(lifetime::on, [this](auto) { process(key::C);     }, -100, false));
-			keep(act->bind<kb::key<key::D    >>(lifetime::on, [this](auto) { process(key::D);     }, -100, false));
-			keep(act->bind<kb::key<key::E    >>(lifetime::on, [this](auto) { process(key::E);     }, -100, false));
-			keep(act->bind<kb::key<key::F    >>(lifetime::on, [this](auto) { process(key::F);     }, -100, false));
-			keep(act->bind<kb::key<key::G    >>(lifetime::on, [this](auto) { process(key::G);     }, -100, false));
-			keep(act->bind<kb::key<key::H    >>(lifetime::on, [this](auto) { process(key::H);     }, -100, false));
-			keep(act->bind<kb::key<key::I    >>(lifetime::on, [this](auto) { process(key::I);     }, -100, false));
-			keep(act->bind<kb::key<key::J    >>(lifetime::on, [this](auto) { process(key::J);     }, -100, false));
-			keep(act->bind<kb::key<key::K    >>(lifetime::on, [this](auto) { process(key::K);     }, -100, false));
-			keep(act->bind<kb::key<key::L    >>(lifetime::on, [this](auto) { process(key::L);     }, -100, false));
-			keep(act->bind<kb::key<key::M    >>(lifetime::on, [this](auto) { process(key::M);     }, -100, false));
-			keep(act->bind<kb::key<key::N    >>(lifetime::on, [this](auto) { process(key::N);     }, -100, false));
-			keep(act->bind<kb::key<key::O    >>(lifetime::on, [this](auto) { process(key::O);     }, -100, false));
-			keep(act->bind<kb::key<key::P    >>(lifetime::on, [this](auto) { process(key::P);     }, -100, false));
-			keep(act->bind<kb::key<key::Q    >>(lifetime::on, [this](auto) { process(key::Q);     }, -100, false));
-			keep(act->bind<kb::key<key::R    >>(lifetime::on, [this](auto) { process(key::R);     }, -100, false));
-			keep(act->bind<kb::key<key::S    >>(lifetime::on, [this](auto) { process(key::S);     }, -100, false));
-			keep(act->bind<kb::key<key::T    >>(lifetime::on, [this](auto) { process(key::T);     }, -100, false));
-			keep(act->bind<kb::key<key::U    >>(lifetime::on, [this](auto) { process(key::U);     }, -100, false));
-			keep(act->bind<kb::key<key::V    >>(lifetime::on, [this](auto) { process(key::V);     }, -100, false));
-			keep(act->bind<kb::key<key::W    >>(lifetime::on, [this](auto) { process(key::W);     }, -100, false));
-			keep(act->bind<kb::key<key::X    >>(lifetime::on, [this](auto) { process(key::X);     }, -100, false));
-			keep(act->bind<kb::key<key::Y    >>(lifetime::on, [this](auto) { process(key::Y);     }, -100, false));
-			keep(act->bind<kb::key<key::Z    >>(lifetime::on, [this](auto) { process(key::Z);     }, -100, false));
-			keep(act->bind<kb::key<key::Space>>(lifetime::on, [this](auto) { process(key::Space); }, -100, false));
-			keep(act->bind<kb::key<key::Enter>>(lifetime::on, [this](auto) { process(key::Enter); }, -100, false));
+			keep(act->bind<kb::key<key::A        >>(lifetime::on, [this](auto) { handle(key::A);         }, -100, false));
+			keep(act->bind<kb::key<key::B        >>(lifetime::on, [this](auto) { handle(key::B);         }, -100, false));
+			keep(act->bind<kb::key<key::C        >>(lifetime::on, [this](auto) { handle(key::C);         }, -100, false));
+			keep(act->bind<kb::key<key::D        >>(lifetime::on, [this](auto) { handle(key::D);         }, -100, false));
+			keep(act->bind<kb::key<key::E        >>(lifetime::on, [this](auto) { handle(key::E);         }, -100, false));
+			keep(act->bind<kb::key<key::F        >>(lifetime::on, [this](auto) { handle(key::F);         }, -100, false));
+			keep(act->bind<kb::key<key::G        >>(lifetime::on, [this](auto) { handle(key::G);         }, -100, false));
+			keep(act->bind<kb::key<key::H        >>(lifetime::on, [this](auto) { handle(key::H);         }, -100, false));
+			keep(act->bind<kb::key<key::I        >>(lifetime::on, [this](auto) { handle(key::I);         }, -100, false));
+			keep(act->bind<kb::key<key::J        >>(lifetime::on, [this](auto) { handle(key::J);         }, -100, false));
+			keep(act->bind<kb::key<key::K        >>(lifetime::on, [this](auto) { handle(key::K);         }, -100, false));
+			keep(act->bind<kb::key<key::L        >>(lifetime::on, [this](auto) { handle(key::L);         }, -100, false));
+			keep(act->bind<kb::key<key::M        >>(lifetime::on, [this](auto) { handle(key::M);         }, -100, false));
+			keep(act->bind<kb::key<key::N        >>(lifetime::on, [this](auto) { handle(key::N);         }, -100, false));
+			keep(act->bind<kb::key<key::O        >>(lifetime::on, [this](auto) { handle(key::O);         }, -100, false));
+			keep(act->bind<kb::key<key::P        >>(lifetime::on, [this](auto) { handle(key::P);         }, -100, false));
+			keep(act->bind<kb::key<key::Q        >>(lifetime::on, [this](auto) { handle(key::Q);         }, -100, false));
+			keep(act->bind<kb::key<key::R        >>(lifetime::on, [this](auto) { handle(key::R);         }, -100, false));
+			keep(act->bind<kb::key<key::S        >>(lifetime::on, [this](auto) { handle(key::S);         }, -100, false));
+			keep(act->bind<kb::key<key::T        >>(lifetime::on, [this](auto) { handle(key::T);         }, -100, false));
+			keep(act->bind<kb::key<key::U        >>(lifetime::on, [this](auto) { handle(key::U);         }, -100, false));
+			keep(act->bind<kb::key<key::V        >>(lifetime::on, [this](auto) { handle(key::V);         }, -100, false));
+			keep(act->bind<kb::key<key::W        >>(lifetime::on, [this](auto) { handle(key::W);         }, -100, false));
+			keep(act->bind<kb::key<key::X        >>(lifetime::on, [this](auto) { handle(key::X);         }, -100, false));
+			keep(act->bind<kb::key<key::Y        >>(lifetime::on, [this](auto) { handle(key::Y);         }, -100, false));
+			keep(act->bind<kb::key<key::Z        >>(lifetime::on, [this](auto) { handle(key::Z);         }, -100, false));
+			keep(act->bind<kb::key<key::Num1     >>(lifetime::on, [this](auto) { handle(key::Num1);      }, -100, false));
+			keep(act->bind<kb::key<key::Num2     >>(lifetime::on, [this](auto) { handle(key::Num2);      }, -100, false));
+			keep(act->bind<kb::key<key::Num3     >>(lifetime::on, [this](auto) { handle(key::Num3);      }, -100, false));
+			keep(act->bind<kb::key<key::Num4     >>(lifetime::on, [this](auto) { handle(key::Num4);      }, -100, false));
+			keep(act->bind<kb::key<key::Num5     >>(lifetime::on, [this](auto) { handle(key::Num5);      }, -100, false));
+			keep(act->bind<kb::key<key::Num6     >>(lifetime::on, [this](auto) { handle(key::Num6);      }, -100, false));
+			keep(act->bind<kb::key<key::Num7     >>(lifetime::on, [this](auto) { handle(key::Num7);      }, -100, false));
+			keep(act->bind<kb::key<key::Num8     >>(lifetime::on, [this](auto) { handle(key::Num8);      }, -100, false));
+			keep(act->bind<kb::key<key::Num9     >>(lifetime::on, [this](auto) { handle(key::Num9);      }, -100, false));
+			keep(act->bind<kb::key<key::Num0     >>(lifetime::on, [this](auto) { handle(key::Num0);      }, -100, false));
+			keep(act->bind<kb::key<key::Hyphen   >>(lifetime::on, [this](auto) { handle(key::Hyphen);    }, -100, false));
+			keep(act->bind<kb::key<key::Equals   >>(lifetime::on, [this](auto) { handle(key::Equals);    }, -100, false));
+			keep(act->bind<kb::key<key::Comma    >>(lifetime::on, [this](auto) { handle(key::Comma);     }, -100, false));
+			keep(act->bind<kb::key<key::Period   >>(lifetime::on, [this](auto) { handle(key::Period);    }, -100, false));
+			keep(act->bind<kb::key<key::Space    >>(lifetime::on, [this](auto) { handle(key::Space);     }, -100, false));
+			keep(act->bind<kb::key<key::Enter    >>(lifetime::on, [this](auto) { handle(key::Enter);     }, -100, false));
+			keep(act->bind<kb::key<key::Backspace>>(lifetime::on, [this](auto) { handle(key::Backspace); }, -100, false));
 
-			keep(act->bind<kb::key<key::A    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::B    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::C    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::D    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::E    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::F    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::G    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::H    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::I    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::J    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::K    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::L    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::M    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::N    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::O    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::P    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::Q    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::R    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::S    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::T    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::U    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::V    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::W    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::X    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::Y    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::Z    >>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::Space>>(lifetime::when, [this](auto) {}, -100, false));
-			keep(act->bind<kb::key<key::Enter>>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::A        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::B        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::C        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::D        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::E        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::F        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::G        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::H        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::I        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::J        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::K        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::L        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::M        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::N        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::O        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::P        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Q        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::R        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::S        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::T        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::U        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::V        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::W        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::X        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Y        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Z        >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num1     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num2     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num3     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num4     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num5     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num6     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num7     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num8     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num9     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Num0     >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Hyphen   >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Equals   >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Comma    >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Period   >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Space    >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Enter    >>(lifetime::when, [this](auto) {}, -100, false));
+			keep(act->bind<kb::key<key::Backspace>>(lifetime::when, [this](auto) {}, -100, false));
 		}
 	  public:
 		static bool supported() { return true; }
