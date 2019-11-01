d790785992852110f78b9ec646323d2542df24e7
David Farrell <shokku.ra@gmail.com>
2019-11-01 10:26:59 +0000

    Add scale accessors

--- include/polar/component/scale.h
+++ include/polar/component/scale.h
@@ -16,6 +16,35 @@ namespace polar::component {
 			get<property::integrable>().lock()->add(&this->sc);
 		}
 
-		virtual std::string name() const { return "scale"; }
+		virtual std::string name() const override { return "scale"; }
+
+		virtual accessor_list accessors() const override {
+			accessor_list l;
+			l.emplace_back("x", make_accessor<scale>(
+				[] (scale *ptr) {
+					return ptr->sc->x;
+				},
+				[] (scale *ptr, auto x) {
+					ptr->sc->x = x;
+				}
+			));
+			l.emplace_back("y", make_accessor<scale>(
+				[] (scale *ptr) {
+					return ptr->sc->y;
+				},
+				[] (scale *ptr, auto y) {
+					ptr->sc->y = y;
+				}
+			));
+			l.emplace_back("z", make_accessor<scale>(
+				[] (auto ptr) {
+					return ptr->sc->z;
+				},
+				[] (scale *ptr, auto z) {
+					ptr->sc->z = z;
+				}
+			));
+			return l;
+		}
 	};
 } // namespace polar::component
