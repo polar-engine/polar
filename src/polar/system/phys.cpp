b07701afdd14df68b1050cc832d8d7c0b54c086d
David Farrell <shokku.ra@gmail.com>
2019-11-30 18:20:46 +0000

    Add ball-ball resolver, tidy up box-box resolver

--- src/polar/system/phys.cpp
+++ src/polar/system/phys.cpp
@@ -2,6 +2,7 @@
 #include <polar/component/position.h>
 #include <polar/component/scale.h>
 #include <polar/support/phys/detector/box.h>
+#include <polar/support/phys/detector/ball.h>
 #include <polar/system/event.h>
 #include <polar/system/phys.h>
 
@@ -24,9 +25,7 @@ namespace polar::system {
 						auto phys2 = static_cast<component::phys *>(comp2);
 						auto &det1 = *phys1->detector;
 						auto &det2 = *phys2->detector;
-						auto pair =
-						std::make_pair(std::type_index(typeid(det1)),
-						std::type_index(typeid(det2)));
+						auto pair = std::make_pair(std::type_index(typeid(det1)), std::type_index(typeid(det2)));
 						auto search = resolvers.find(pair);
 						if(search != resolvers.cend()) {
 							auto b = search->second->operator()(
@@ -48,34 +47,55 @@ namespace polar::system {
 		};
 		keep(eventM->listenfor("integrator", "ticked", f));
 
-		add<support::phys::detector::box, support::phys::detector::box>(
-		    [](core::polar *engine, auto a, auto b) {
-			    Point3 originA{0};
-			    Point3 originB{0};
-			    if(auto p = engine->get<component::position>(a.id)) {
-				    originA += p->pos.get();
-			    }
-			    if(auto p = engine->get<component::position>(b.id)) {
-				    originB += p->pos.get();
-			    }
+		add<support::phys::detector::box, support::phys::detector::box>([](core::polar *engine, auto a, auto b) {
+			Point3 originA{0};
+			Point3 originB{0};
+			if(auto p = engine->get<component::position>(a.id)) {
+				originA += p->pos.get();
+			}
+			if(auto p = engine->get<component::position>(b.id)) {
+				originB += p->pos.get();
+			}
 
-			    Point3 scaleA = a.detector->size;
-			    Point3 scaleB = b.detector->size;
-			    if(auto s = engine->get<component::scale>(a.id)) {
-				    scaleA *= s->sc.get();
-			    }
-			    if(auto s = engine->get<component::scale>(b.id)) {
-				    scaleB *= s->sc.get();
-			    }
+			Point3 scaleA = a.detector->size;
+			Point3 scaleB = b.detector->size;
+			if(auto s = engine->get<component::scale>(a.id)) {
+				scaleA *= s->sc.get();
+			}
+			if(auto s = engine->get<component::scale>(b.id)) {
+				scaleB *= s->sc.get();
+			}
 
-			    auto minA = originA - scaleA;
-			    auto maxA = originA + scaleA;
-			    auto minB = originB - scaleB;
-			    auto maxB = originB + scaleB;
+			auto minA = originA - scaleA;
+			auto maxA = originA + scaleA;
+			auto minB = originB - scaleB;
+			auto maxB = originB + scaleB;
 
-			    return minA.x <= maxB.x && maxA.x >= minB.x &&
-			           minA.y <= maxB.y && maxA.y >= minB.y &&
-			           minA.z <= maxB.z && maxA.z >= minB.z;
-		    });
+			return minA.x <= maxB.x && maxA.x >= minB.x &&
+			       minA.y <= maxB.y && maxA.y >= minB.y &&
+			       minA.z <= maxB.z && maxA.z >= minB.z;
+		});
+
+		add<support::phys::detector::ball, support::phys::detector::ball>([](core::polar *engine, auto a, auto b) {
+			Point3 originA{0};
+			Point3 originB{0};
+			if(auto p = engine->get<component::position>(a.id)) {
+				originA += p->pos.get();
+			}
+			if(auto p = engine->get<component::position>(b.id)) {
+				originB += p->pos.get();
+			}
+
+			Point3 scaleA = a.detector->size;
+			Point3 scaleB = b.detector->size;
+			if(auto s = engine->get<component::scale>(a.id)) {
+				scaleA *= s->sc.get();
+			}
+			if(auto s = engine->get<component::scale>(b.id)) {
+				scaleB *= s->sc.get();
+			}
+
+			return glm::distance(originA, originB) <= scaleA.x + scaleB.x;
+		});
 	}
 } // namespace polar::system
