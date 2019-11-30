35a9a48e9295fc3ca1f010f9dff4c89040fdc3e4
David Farrell <shokku.ra@gmail.com>
2019-11-30 18:05:16 +0000

    Add new math constants

--- include/polar/math/constants.h
+++ include/polar/math/constants.h
@@ -1,6 +1,8 @@
 #pragma once
 
 namespace polar::math {
-	constexpr double PI     = 3.14159265358979323846264338327950288419716939937510;
-	constexpr double TWO_PI = PI * 2.0;
+	constexpr double PI          = 3.14159265358979323846264338327950288419716939937510;
+	constexpr double TWO_PI      = PI * 2.0;
+	constexpr double PI_OVER_TWO = PI / 2.0;
+	constexpr double PI_OVER_TEN = PI / 10.0;
 } // namespace polar::math
