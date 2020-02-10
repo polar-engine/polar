9f4494b2ae601b243d3e574a886399bf865e55d7
David Farrell <shokku.ra@gmail.com>
2020-02-10 15:37:43 +0000

    clang-format

--- src/polar/system/integrator.cpp
+++ src/polar/system/integrator.cpp
@@ -5,13 +5,10 @@
 
 namespace polar::system {
 	void integrator::tick(DeltaTicks::seconds_type seconds) {
-		for(auto it = engine->objects.left.begin();
-		    it != engine->objects.left.end(); ++it) {
+		for(auto it = engine->objects.left.begin(); it != engine->objects.left.end(); ++it) {
 			auto property = it->info->get<property::integrable>().lock();
 			if(property) {
-				for(auto integrable : *property->get()) {
-					integrable->integrate(seconds);
-				}
+				for(auto integrable : *property->get()) { integrable->integrate(seconds); }
 			}
 		}
 		engine->get<event>().lock()->firein("integrator", "ticked", seconds);
@@ -20,26 +17,20 @@ namespace polar::system {
 	void integrator::revert_by(size_t n) {
 		if(n == 0) { return; }
 
-		for(auto it = engine->objects.left.begin();
-		    it != engine->objects.left.end(); ++it) {
+		for(auto it = engine->objects.left.begin(); it != engine->objects.left.end(); ++it) {
 			auto property = it->info->get<property::integrable>().lock();
 			if(property) {
-				for(auto integrable : *property->get()) {
-					integrable->revert_by(n);
-				}
+				for(auto integrable : *property->get()) { integrable->revert_by(n); }
 			}
 		}
 	}
 
 	void integrator::revert_to(size_t n) {
-		for(auto it = engine->objects.left.begin();
-		    it != engine->objects.left.end(); ++it) {
+		for(auto it = engine->objects.left.begin(); it != engine->objects.left.end(); ++it) {
 			auto property = it->info->get<property::integrable>().lock();
 			if(property) {
-				for(auto integrable : *property->get()) {
-					integrable->revert_to(n);
-				}
+				for(auto integrable : *property->get()) { integrable->revert_to(n); }
 			}
 		}
 	}
-}
+} // namespace polar::system
