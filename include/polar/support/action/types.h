4af031181e34086d8bc8e77ed4efc82c590c2309
David Farrell <shokku.ra@gmail.com>
2020-02-12 10:05:55 +0000

    Fix compilation error

--- include/polar/support/action/types.h
+++ include/polar/support/action/types.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <polar/core/ref.h>
 
 namespace polar::support::action {
@@ -13,20 +14,20 @@ namespace polar::support::action {
 	using analog_cont_t      = std::function<bool(core::weak_ref, math::decimal)>;
 
 	struct digital_data {
-		std::unordered_map<core::ref, bool> states;
+		std::map<core::ref, bool> states;
 	};
 
 	struct analog_state {
-		math::decimal initial = 0;
+		math::decimal initial  = 0;
 		math::decimal previous = 0;
-		math::decimal value = 0;
-		math::decimal saved = 0;
+		math::decimal value    = 0;
+		math::decimal saved    = 0;
 	};
 
 	struct analog_data {
-		std::unordered_map<core::ref, analog_state> states;
+		std::map<core::ref, analog_state> states;
 	};
 
-	using digital_map = std::unordered_map<std::type_index, digital_data>;
-	using analog_map  = std::unordered_map<std::type_index, analog_data>;
-}
+	using digital_map = std::map<std::type_index, digital_data>;
+	using analog_map  = std::map<std::type_index, analog_data>;
+} // namespace polar::support::action
